e5a240074b9d7debe8dfa785e7a8101adb43b4cf
Nikolay Kurtov <nkurtov@ulvetanna.io>
2024-08-30 20:27:11 +0200

    compiles

--- crates/hal/src/immutable_slice.rs
+++ crates/hal/src/immutable_slice.rs
@@ -1,45 +1,55 @@
 // Copyright 2024 Ulvetanna Inc.
 
-#[cfg(not(feature = "linerate-backend"))]
 use binius_field::PackedField;
-#[cfg(not(feature = "linerate-backend"))]
+#[cfg(feature = "linerate-backend")]
+use binius_linerate_slice::ImmutableSlice;
 use std::ops::{Deref, DerefMut};
 
 /// Wrapper for compatibility between Vec and ImmutableSlice.
-#[cfg(not(feature = "linerate-backend"))]
+#[allow(dead_code)]
 #[derive(Debug)]
 pub enum VecOrImmutableSlice<P> {
 	V(Vec<P>),
+	#[cfg(feature = "linerate-backend")]
+	// Supports only read-only operations.
+	IS(ImmutableSlice<P>),
 }
 
-#[cfg(not(feature = "linerate-backend"))]
 impl<P> Deref for VecOrImmutableSlice<P> {
 	type Target = [P];
 
 	fn deref(&self) -> &Self::Target {
 		match self {
 			VecOrImmutableSlice::V(v) => v,
+			#[cfg(feature = "linerate-backend")]
+			VecOrImmutableSlice::IS(s) => s,
 		}
 	}
 }
 
-#[cfg(not(feature = "linerate-backend"))]
 impl<P: PackedField> VecOrImmutableSlice<P> {
+	#[allow(dead_code)]
 	pub fn into_expansion(self, len: usize) -> VecOrImmutableSlice<P> {
 		match self {
 			VecOrImmutableSlice::V(mut v) => {
 				v.resize(len, P::zero());
 				VecOrImmutableSlice::V(v)
 			}
+			#[cfg(feature = "linerate-backend")]
+			VecOrImmutableSlice::IS(ref is) => {
+				assert_eq!(is.len(), len);
+				self
+			}
 		}
 	}
 }
 
-#[cfg(not(feature = "linerate-backend"))]
 impl<P> DerefMut for VecOrImmutableSlice<P> {
 	fn deref_mut(&mut self) -> &mut Self::Target {
 		match self {
 			VecOrImmutableSlice::V(v) => v,
+			#[cfg(feature = "linerate-backend")]
+			VecOrImmutableSlice::IS(_) => panic!("Unsupported"),
 		}
 	}
 }
