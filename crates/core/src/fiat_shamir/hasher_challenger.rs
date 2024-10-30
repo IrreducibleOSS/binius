c10473320a5ca4624ead4c403c09f3e5bc7597ae
Anex Johnson <ajohnson@ulvetanna.io>
2024-10-30 11:46:41 +0000

    [fiat_shamir] Created `Buf` and `BufMut` challengers

--- crates/core/src/fiat_shamir/hasher_challenger.rs
+++ crates/core/src/fiat_shamir/hasher_challenger.rs
@@ -1,33 +1,97 @@
 // Copyright 2024 Irreducible Inc.
 
-use digest::{Digest, FixedOutputReset, Output};
-use p3_challenger::{CanObserve, CanSample, CanSampleBits};
-use std::slice;
+use bytes::{buf::UninitSlice, Buf, BufMut};
+use digest::{
+	core_api::{Block, BlockSizeUser},
+	Digest, FixedOutputReset, Output,
+};
+use std::cmp::min;
 
-/// Challenger interface over hashes that implement `[Digest]` trait,
-///
-/// This challenger works over bytes instead of Field elements
+/// Challenger type which implements `[Buf]` that has similar functionality as `[CanSample]`
 #[derive(Debug)]
-pub struct HasherChallenger<H: Digest> {
+pub struct HasherSampler<H: Digest> {
 	index: usize,
 	buffer: Output<H>,
 	hasher: H,
 }
 
+/// Challenger type which implements `[BufMut]` that has similar functionality as `[CanObserve]`
+#[derive(Debug)]
+pub struct HasherObserver<H: Digest + BlockSizeUser> {
+	index: usize,
+	buffer: Block<H>,
+	hasher: H,
+}
+
+/// Challenger interface over hashes that implement `[Digest]` trait,
+///
+/// This challenger works over bytes instead of Field elements
+#[derive(Debug)]
+pub enum HasherChallenger<H: Digest + BlockSizeUser> {
+	Observer(HasherObserver<H>),
+	Sampler(HasherSampler<H>),
+}
+
 impl<H> Default for HasherChallenger<H>
 where
-	H: Digest,
+	H: Digest + BlockSizeUser,
 {
 	fn default() -> Self {
-		Self {
+		Self::Observer(HasherObserver {
 			hasher: H::new(),
-			index: <H as Digest>::output_size(),
-			buffer: Output::<H>::default(),
+			index: 0,
+			buffer: Block::<H>::default(),
+		})
+	}
+}
+
+impl<H: Digest + BlockSizeUser + FixedOutputReset + Default> HasherChallenger<H> {
+	/// This returns the inner challenger which implements `[BufMut]`
+	pub fn observer(&mut self) -> &mut HasherObserver<H> {
+		match self {
+			Self::Observer(observer) => observer,
+			Self::Sampler(sampler) => {
+				*self = Self::Observer(sampler.into_observer());
+				match self {
+					Self::Observer(observer) => observer,
+					_ => unreachable!(),
+				}
+			}
+		}
+	}
+
+	/// This returns the inner challenger which implements `[Buf]`
+	pub fn sampler(&mut self) -> &mut HasherSampler<H> {
+		match self {
+			Self::Sampler(sampler) => sampler,
+			Self::Observer(observer) => {
+				*self = Self::Sampler(observer.into_sampler());
+				match self {
+					Self::Sampler(sampler) => sampler,
+					_ => unreachable!(),
+				}
+			}
 		}
 	}
 }
 
-impl<H> HasherChallenger<H>
+impl<H> HasherSampler<H>
+where
+	H: Digest + Default + BlockSizeUser,
+{
+	#[allow(clippy::wrong_self_convention)]
+	fn into_observer(&mut self) -> HasherObserver<H> {
+		Digest::update(&mut self.hasher, self.index.to_le_bytes());
+
+		HasherObserver {
+			hasher: std::mem::take(&mut self.hasher),
+			index: 0,
+			buffer: Block::<H>::default(),
+		}
+	}
+}
+
+impl<H> HasherSampler<H>
 where
 	H: Digest + FixedOutputReset,
 {
@@ -38,68 +102,90 @@ where
 		Digest::update(&mut self.hasher, &digest);
 
 		self.buffer = digest;
+		self.index = 0
 	}
 }
 
-impl<H> CanObserve<u8> for HasherChallenger<H>
+impl<H> HasherObserver<H>
 where
-	H: Digest,
+	H: Digest + BlockSizeUser + Default,
 {
-	fn observe(&mut self, value: u8) {
-		self.observe_slice(slice::from_ref(&value))
-	}
-
-	fn observe_slice(&mut self, values: &[u8]) {
-		let rate = <H as Digest>::output_size();
-		if self.index != 0 && self.index != rate {
-			Digest::update(&mut self.hasher, self.index.to_le_bytes());
+	#[allow(clippy::wrong_self_convention)]
+	fn into_sampler(&mut self) -> HasherSampler<H> {
+		self.flush();
+		HasherSampler {
+			hasher: std::mem::take(&mut self.hasher),
+			index: <H as Digest>::output_size(),
+			buffer: Output::<H>::default(),
 		}
-
-		self.index = rate;
-
-		Digest::update(&mut self.hasher, values);
 	}
 }
 
-impl<H> CanSample<u8> for HasherChallenger<H>
+impl<H> HasherObserver<H>
 where
-	H: Digest + FixedOutputReset,
+	H: Digest + BlockSizeUser,
 {
-	fn sample(&mut self) -> u8 {
-		if self.index == self.buffer.len() {
+	fn flush(&mut self) {
+		self.hasher.update(&self.buffer[..self.index]);
+		self.index = 0
+	}
+}
+
+impl<H> Buf for HasherSampler<H>
+where
+	H: Digest + FixedOutputReset + Default,
+{
+	fn remaining(&self) -> usize {
+		usize::MAX
+	}
+
+	fn chunk(&self) -> &[u8] {
+		&self.buffer[self.index..]
+	}
+
+	fn advance(&mut self, mut cnt: usize) {
+		if self.index == <H as Digest>::output_size() {
 			self.fill_buffer();
-			self.index = 0;
 		}
 
-		let sampled = self.buffer[self.index];
-		self.index += 1;
-
-		sampled
+		while cnt > 0 {
+			let remaining = min(<H as Digest>::output_size() - self.index, cnt);
+			if remaining == 0 {
+				self.fill_buffer();
+				continue;
+			}
+			cnt -= remaining;
+			self.index += remaining;
+		}
 	}
 }
 
-impl<H> CanSampleBits<usize> for HasherChallenger<H>
+unsafe impl<H> BufMut for HasherObserver<H>
 where
-	H: Digest + FixedOutputReset,
+	H: Digest + BlockSizeUser,
 {
-	fn sample_bits(&mut self, bits: usize) -> usize {
-		let bits = bits.min(usize::BITS as usize);
+	fn remaining_mut(&self) -> usize {
+		usize::MAX
+	}
 
-		let bytes_to_sample = (bits + 7) / 8;
-
-		let mut bytes = [0u8; std::mem::size_of::<usize>()];
-
-		for byte in bytes.iter_mut().take(bytes_to_sample) {
-			*byte = self.sample();
+	unsafe fn advance_mut(&mut self, mut cnt: usize) {
+		if self.index == <H as BlockSizeUser>::block_size() {
+			self.flush();
 		}
 
-		let unmasked = usize::from_le_bytes(bytes);
-		let mask = 1usize.checked_shl(bits as u32);
-		let mask = match mask {
-			Some(x) => x - 1,
-			None => usize::MAX,
-		};
-		mask & unmasked
+		while cnt > 0 {
+			let remaining = min(<H as BlockSizeUser>::block_size() - self.index, cnt);
+			cnt -= remaining;
+			self.index += remaining;
+			if remaining == <H as BlockSizeUser>::block_size() {
+				self.flush();
+			}
+		}
+	}
+
+	fn chunk_mut(&mut self) -> &mut UninitSlice {
+		let buffer = &mut self.buffer[self.index..];
+		buffer.into()
 	}
 }
 
@@ -107,29 +193,79 @@ where
 mod tests {
 	use super::*;
 	use groestl_crypto::Groestl256;
+	use rand::{thread_rng, RngCore};
 
 	#[test]
-	fn test_hasher_challenger() {
+	fn test_starting_sampler() {
 		let mut hasher = Groestl256::default();
 		let mut challenger = HasherChallenger::<Groestl256>::default();
-		let out: [u8; 8] = challenger.sample_array();
+		let mut out = [0u8; 8];
+		challenger.sampler().copy_to_slice(&mut out);
 
 		let first_hash_out = hasher.finalize_reset();
 		hasher.update(first_hash_out);
 
 		assert_eq!(first_hash_out[0..8], out);
 
-		let out: [u8; 24] = challenger.sample_array();
+		let mut out = [0u8; 24];
+		challenger.sampler().copy_to_slice(&mut out);
 		assert_eq!(first_hash_out[8..], out);
 
-		challenger.observe_slice(&[0x48, 0x55]);
+		challenger.observer().put_slice(&[0x48, 0x55]);
+		hasher.update([32, 0, 0, 0, 0, 0, 0, 0]);
 		hasher.update([0x48, 0x55]);
 
-		let out_after_observe: [u8; 2] = challenger.sample_array();
+		let mut out_after_observe = [0u8; 2];
+		challenger.sampler().copy_to_slice(&mut out_after_observe);
 
 		let second_hash_out = hasher.finalize_reset();
-		hasher.update(second_hash_out);
 
 		assert_eq!(out_after_observe, second_hash_out[..2]);
+	}
+
+	#[test]
+	fn test_starting_observer() {
+		let mut hasher = Groestl256::default();
+		let mut challenger = HasherChallenger::<Groestl256>::default();
+		let mut observable = [0u8; 1019];
+		thread_rng().fill_bytes(&mut observable);
+		challenger.observer().put_slice(&observable[..39]);
+		challenger.observer().put_slice(&observable[39..300]);
+		challenger.observer().put_slice(&observable[300..987]);
+		challenger.observer().put_slice(&observable[987..]);
+		hasher.update(observable);
+
+		let mut out = [0u8; 7];
+		challenger.sampler().copy_to_slice(&mut out);
+
+		let first_hash_out = hasher.finalize_reset();
+		hasher.update(first_hash_out);
+
+		assert_eq!(first_hash_out[..7], out);
+
+		thread_rng().fill_bytes(&mut observable);
+		challenger.observer().put_slice(&observable[..128]);
+		// updated with index of sampler first
+		hasher.update([7, 0, 0, 0, 0, 0, 0, 0]);
+
+		hasher.update(&observable[..128]);
+
+		let mut out = [0u8; 32];
+		challenger.sampler().copy_to_slice(&mut out);
+
+		let second_hasher_out = hasher.finalize_reset();
+		hasher.update(second_hasher_out);
+
+		assert_eq!(second_hasher_out[..], out);
+
+		challenger.observer().put_slice(&observable[128..]);
+		hasher.update([32, 0, 0, 0, 0, 0, 0, 0]);
+		hasher.update(&observable[128..]);
+
+		let mut out_again = [0u8; 7];
+		challenger.sampler().copy_to_slice(&mut out_again);
+
+		let final_hasher_out = hasher.finalize_reset();
+		assert_eq!(final_hasher_out[..7], out_again);
 	}
 }
