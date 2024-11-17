c97efa19262201949eacb5f4c220d35749ddf826
Dmitry Gordon <dgordon@irreducible.com>
2024-11-17 14:17:32 +0000

    [cicd] Use the same profile to run benchmarks and tests

--- scripts/run_tests_and_examples.sh
+++ scripts/run_tests_and_examples.sh
@@ -3,16 +3,17 @@
 set -e
 
 export RUST_BACKTRACE=full
+CARGO_PROFILE="${CARGO_PROFILE:-test}"
 
-cargo test
+cargo test --profile $CARGO_PROFILE
 
-# First compile all the examples in release mode to speed up overall compilation time due to parallelism.
-cargo build --release --examples
+# First compile all the examples to speed up overall compilation time due to parallelism.
+cargo build --profile $CARGO_PROFILE --examples
 
 # Execute examples. 
 # Unfortunately there cargo doesn't support executing all examples with a single command.
 # Cargo plugins such as "cargo-examples" do suport it but without a possibility to specify "release" profile.
 for example in examples/*.rs
 do
-    cargo run --example "$(basename "${example%.rs}")"  --release
+    cargo run --profile $CARGO_PROFILE --example "$(basename "${example%.rs}")"
 done
