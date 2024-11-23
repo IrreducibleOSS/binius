b1c5433f7e4d18eee504c6991e71b5d0e912e864
Jacob Kirmayer <jkirmayer@irreducible.com>
2024-11-23 20:45:35 +0000

    [circuits] Two-column return from u8mul

--- crates/circuits/src/lasso/u8mul.rs
+++ crates/circuits/src/lasso/u8mul.rs
@@ -20,6 +20,114 @@ type B32 = BinaryField32b;
 
 const T_LOG_SIZE: usize = 16;
 
+pub fn u8mul_bytesliced<U, F, FBase>(
+	builder: &mut ConstraintSystemBuilder<U, F, FBase>,
+	name: impl ToString + Clone,
+	mult_a: OracleId,
+	mult_b: OracleId,
+	log_size: usize,
+) -> Result<[OracleId; 2], anyhow::Error>
+where
+	U: Pod
+		+ UnderlierType
+		+ PackScalar<B8>
+		+ PackScalar<B16>
+		+ PackScalar<B32>
+		+ PackScalar<F>
+		+ PackScalar<FBase>,
+	PackedType<U, B8>: PackedFieldIndexable,
+	PackedType<U, B16>: PackedFieldIndexable,
+	PackedType<U, B32>: PackedFieldIndexable,
+	F: TowerField
+		+ BinaryField
+		+ ExtensionField<B8>
+		+ ExtensionField<B16>
+		+ ExtensionField<B32>
+		+ ExtensionField<FBase>,
+	FBase: TowerField,
+{
+	builder.push_namespace(name.clone());
+
+	let product = builder.add_committed_multiple("product", log_size, B8::TOWER_LEVEL);
+
+	let lookup_t = builder.add_committed("lookup_t", T_LOG_SIZE, B32::TOWER_LEVEL);
+
+	let lookup_u = builder.add_linear_combination(
+		"lookup_u",
+		log_size,
+		[
+			(mult_a, <F as TowerField>::basis(3, 3)?),
+			(mult_b, <F as TowerField>::basis(3, 2)?),
+			(product[1], <F as TowerField>::basis(3, 1)?),
+			(product[0], <F as TowerField>::basis(3, 0)?),
+		],
+	)?;
+
+	let channel = builder.add_channel();
+
+	let mut u_to_t_mapping = None;
+
+	if let Some(witness) = builder.witness() {
+		let mut product_low_witness = witness.new_column::<B8>(product[0]);
+		let mut product_high_witness = witness.new_column::<B8>(product[1]);
+		let mut lookup_u_witness = witness.new_column::<B32>(lookup_u);
+		let mut lookup_t_witness = witness.new_column::<B32>(lookup_t);
+		let mut u_to_t_mapping_witness = vec![0; 1 << log_size];
+
+		let mult_a_ints = witness.get::<B8>(mult_a)?.as_slice::<u8>();
+		let mult_b_ints = witness.get::<B8>(mult_b)?.as_slice::<u8>();
+
+		let product_low_u8 = product_low_witness.as_mut_slice::<u8>();
+		let product_high_u8 = product_high_witness.as_mut_slice::<u8>();
+		let lookup_u_u32 = lookup_u_witness.as_mut_slice::<u32>();
+		let lookup_t_u32 = lookup_t_witness.as_mut_slice::<u32>();
+
+		for (a, b, lookup_u, product_low, product_high, u_to_t) in izip!(
+			mult_a_ints,
+			mult_b_ints,
+			lookup_u_u32.iter_mut(),
+			product_low_u8.iter_mut(),
+			product_high_u8.iter_mut(),
+			u_to_t_mapping_witness.iter_mut()
+		) {
+			let a_int = *a as usize;
+			let b_int = *b as usize;
+			let ab_product = a_int * b_int;
+			let lookup_index = a_int << 8 | b_int;
+			*lookup_u = (lookup_index << 16 | ab_product) as u32;
+
+			*product_high = (ab_product >> 8) as u8;
+			*product_low = (ab_product & 0xff) as u8;
+
+			*u_to_t = lookup_index;
+		}
+
+		for (i, lookup_t) in lookup_t_u32.iter_mut().enumerate() {
+			let a_int = (i >> 8) & 0xff;
+			let b_int = i & 0xff;
+			let ab_product = a_int * b_int;
+			let lookup_index = a_int << 8 | b_int;
+			assert_eq!(lookup_index, i);
+			*lookup_t = (lookup_index << 16 | ab_product) as u32;
+		}
+
+		u_to_t_mapping = Some(u_to_t_mapping_witness);
+	}
+
+	lasso::<_, _, _, B32, B32, T_LOG_SIZE>(
+		builder,
+		format!("{} lasso", name.to_string()),
+		log_size,
+		u_to_t_mapping,
+		lookup_u,
+		lookup_t,
+		channel,
+	)?;
+
+	builder.pop_namespace();
+	Ok(product)
+}
+
 pub fn u8mul<U, F, FBase>(
 	builder: &mut ConstraintSystemBuilder<U, F, FBase>,
 	name: impl ToString + Clone,
@@ -48,75 +156,32 @@ where
 {
 	builder.push_namespace(name.clone());
 
-	let product = builder.add_committed("product", log_size, B16::TOWER_LEVEL);
+	let product_bytesliced = u8mul_bytesliced(builder, name, mult_a, mult_b, log_size)?;
 
-	let lookup_t = builder.add_committed("lookup_t", T_LOG_SIZE, B32::TOWER_LEVEL);
-
-	let lookup_u = builder.add_linear_combination(
-		"lookup_u",
+	let product = builder.add_linear_combination(
+		"bytes summed",
 		log_size,
 		[
-			(mult_a, <F as TowerField>::basis(3, 3)?),
-			(mult_b, <F as TowerField>::basis(3, 2)?),
-			(product, <F as TowerField>::basis(3, 0)?),
+			(product_bytesliced[0], <F as TowerField>::basis(3, 0)?),
+			(product_bytesliced[1], <F as TowerField>::basis(3, 1)?),
 		],
 	)?;
 
-	let channel = builder.add_channel();
-
-	let mut u_to_t_mapping = None;
-
 	if let Some(witness) = builder.witness() {
+		let product_low_witness = witness.get::<B8>(product_bytesliced[0])?;
+		let product_high_witness = witness.get::<B8>(product_bytesliced[1])?;
+
 		let mut product_witness = witness.new_column::<B16>(product);
-		let mut lookup_u_witness = witness.new_column::<B32>(lookup_u);
-		let mut lookup_t_witness = witness.new_column::<B32>(lookup_t);
-		let mut u_to_t_mapping_witness = vec![0; 1 << log_size];
 
-		let mult_a_ints = witness.get::<B8>(mult_a)?.as_slice::<u8>();
-		let mult_b_ints = witness.get::<B8>(mult_b)?.as_slice::<u8>();
+		let product_low_u8 = product_low_witness.as_slice::<u8>();
+		let product_high_u8 = product_high_witness.as_slice::<u8>();
 
-		let product_scalars = PackedType::<U, B16>::unpack_scalars_mut(product_witness.packed());
-		let lookup_u_scalars = PackedType::<U, B32>::unpack_scalars_mut(lookup_u_witness.packed());
-		let lookup_t_scalars = PackedType::<U, B32>::unpack_scalars_mut(lookup_t_witness.packed());
+		let product_u16 = product_witness.as_mut_slice::<u16>();
 
-		for (a, b, lookup_u, product, u_to_t) in izip!(
-			mult_a_ints,
-			mult_b_ints,
-			lookup_u_scalars.iter_mut(),
-			product_scalars.iter_mut(),
-			u_to_t_mapping_witness.iter_mut()
-		) {
-			let a_int = *a as usize;
-			let b_int = *b as usize;
-			let ab_product = a_int * b_int;
-			let lookup_index = a_int << 8 | b_int;
-			*lookup_u = B32::new((lookup_index << 16 | ab_product) as u32);
-
-			*product = B16::new(ab_product as u16);
-			*u_to_t = lookup_index;
+		for (row_idx, row_product) in product_u16.iter_mut().enumerate() {
+			*row_product = (product_high_u8[row_idx] as u16) << 8 | product_low_u8[row_idx] as u16;
 		}
-
-		for (i, lookup_t) in lookup_t_scalars.iter_mut().enumerate() {
-			let a_int = (i >> 8) & 0xff;
-			let b_int = i & 0xff;
-			let ab_product = a_int * b_int;
-			let lookup_index = a_int << 8 | b_int;
-			assert_eq!(lookup_index, i);
-			*lookup_t = B32::new((lookup_index << 16 | ab_product) as u32);
-		}
-
-		u_to_t_mapping = Some(u_to_t_mapping_witness);
 	}
-
-	lasso::<_, _, _, B32, B32, T_LOG_SIZE>(
-		builder,
-		format!("{} lasso", name.to_string()),
-		log_size,
-		u_to_t_mapping,
-		lookup_u,
-		lookup_t,
-		channel,
-	)?;
 
 	builder.pop_namespace();
 	Ok(product)
