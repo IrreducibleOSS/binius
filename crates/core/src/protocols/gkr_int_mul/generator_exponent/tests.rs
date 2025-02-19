95d851b36cc86b79fe46f501b3fa364d6342e09c
Aliaksei Dziadziuk <adziadziuk@irreducible.com>
2025-02-19 14:08:34 +0100

    exponentiation protocol batch prove/verify

--- crates/core/src/protocols/gkr_int_mul/generator_exponent/tests.rs
+++ crates/core/src/protocols/gkr_int_mul/generator_exponent/tests.rs
@@ -14,15 +14,13 @@ use binius_math::{
 	MultilinearQuery, MultilinearQueryRef,
 };
 use groestl_crypto::Groestl256;
-use itertools::izip;
 use rand::{thread_rng, Rng};
 
 use super::{common::GeneratorExponentReductionOutput, prove};
 use crate::{
-	fiat_shamir::{Challenger, HasherChallenger},
-	protocols::{
-		gkr_gpa::LayerClaim,
-		gkr_int_mul::generator_exponent::{verify, witness::GeneratorExponentWitness},
+	fiat_shamir::HasherChallenger,
+	protocols::gkr_int_mul::generator_exponent::{
+		common::GeneratorExponentClaim, verify, witness::GeneratorExponentWitness,
 	},
 	transcript::ProverTranscript,
 };
@@ -33,80 +31,131 @@ type PGenerator = PackedBinaryField2x64b;
 type F = BinaryField128b;
 type PChallenge = PackedBinaryField1x128b;
 
-fn generate_witness_for_testing<const COLUMN_LEN: usize>(
-	exponents_in_each_row: [u64; COLUMN_LEN],
-) -> GeneratorExponentWitness<'static, PBits, PGenerator, PChallenge, 64> {
-	let exponent_witnesses_as_vec: [_; 64] = array::from_fn(|i| {
-		let mut column_witness = vec![PBits::default(); COLUMN_LEN / <PBits as PackedField>::WIDTH];
+fn generate_claim_witness<const COLUMN_LEN: usize>(
+	exponents_in_each_row: [u128; COLUMN_LEN],
+	exponent_bit_width: usize,
+	generator: Option<&[PGenerator]>,
+	eval_point: &[F],
+) -> (GeneratorExponentWitness<'static, PGenerator, PChallenge>, GeneratorExponentClaim<F>) {
+	let exponent_witnesses_as_vec: Vec<_> = (0..exponent_bit_width)
+		.map(|i| {
+			let mut column_witness =
+				vec![PBits::default(); COLUMN_LEN / <PBits as PackedField>::WIDTH];
 
-		for (row, this_row_exponent) in exponents_in_each_row.iter().enumerate() {
-			let this_bit_of_exponent = ((this_row_exponent >> i) & 1) as u8;
+			for (row, this_row_exponent) in exponents_in_each_row.iter().enumerate() {
+				let this_bit_of_exponent = ((this_row_exponent >> i) & 1) as u8;
 
-			let single_bit_value = BinaryField1b::from(SmallU::<1>::new(this_bit_of_exponent));
+				let single_bit_value = BinaryField1b::from(SmallU::<1>::new(this_bit_of_exponent));
 
-			set_packed_slice(&mut column_witness, row, single_bit_value);
-		}
+				set_packed_slice(&mut column_witness, row, single_bit_value);
+			}
 
-		column_witness
-	});
+			column_witness
+		})
+		.collect::<Vec<_>>();
 
-	let exponent_witnesses = exponent_witnesses_as_vec.map(|column| {
-		let mle = MultilinearExtension::from_values(column).unwrap();
-		MLEEmbeddingAdapter::<PBits, PChallenge>::from(mle).upcast_arc_dyn()
-	});
+	let exponent_witnesses = exponent_witnesses_as_vec
+		.into_iter()
+		.map(|column| {
+			let mle = MultilinearExtension::from_values(column).unwrap();
+			MLEEmbeddingAdapter::<PBits, PChallenge>::from(mle).upcast_arc_dyn()
+		})
+		.collect::<Vec<_>>();
 
-	let witness =
-		GeneratorExponentWitness::<'_, PBits, PGenerator, PChallenge, 64>::new(exponent_witnesses)
-			.unwrap();
+	let witness = if let Some(generator) = generator {
+		GeneratorExponentWitness::<'_, PGenerator, PChallenge>::new_with_dynamic_generator(
+			exponent_witnesses,
+			generator,
+		)
+		.unwrap()
+	} else {
+		GeneratorExponentWitness::<'_, PGenerator, PChallenge>::new_with_static_generator(
+			exponent_witnesses,
+		)
+		.unwrap()
+	};
 
-	witness
-}
+	let exponentiation_result = witness.exponentiation_result().unwrap();
 
-fn generate_witness_and_prove<
-	const LOG_SIZE: usize,
-	const COLUMN_LEN: usize,
-	const EXPONENT_BIT_WIDTH: usize,
-	Challenger_,
->(
-	transcript: &mut ProverTranscript<Challenger_>,
-) -> (
-	LayerClaim<F>,
-	GeneratorExponentWitness<'static, PBits, PGenerator, PChallenge, 64>,
-	GeneratorExponentReductionOutput<F, 64>,
-)
-where
-	Challenger_: Challenger,
-{
-	let mut rng = thread_rng();
-
-	let exponent: [u64; COLUMN_LEN] = array::from_fn(|_| rng.gen());
-	let witness = generate_witness_for_testing::<COLUMN_LEN>(exponent);
-
-	let eval_point = [F::default(); LOG_SIZE].map(|_| <F as Field>::random(&mut rng));
-
-	let last_layer = witness.single_bit_output_layers_data[EXPONENT_BIT_WIDTH - 1].clone();
-
-	let last_layer_mle = MultilinearExtension::from_values(last_layer).unwrap();
+	let last_layer_mle = MultilinearExtension::from_values(exponentiation_result.to_vec()).unwrap();
 
 	let last_layer_witness = MLEEmbeddingAdapter::<PGenerator, PChallenge>::from(last_layer_mle);
 
-	let last_layer_query = MultilinearQuery::expand(&eval_point);
+	let last_layer_query = MultilinearQuery::expand(eval_point);
 
-	let claim = LayerClaim {
+	let claim = GeneratorExponentClaim {
 		eval_point: eval_point.to_vec(),
 		eval: last_layer_witness
 			.evaluate(MultilinearQueryRef::new(&last_layer_query))
 			.unwrap(),
+		exponent_bit_width: witness.exponent.len(),
+		with_dynamic_generator: witness.generator.is_some(),
 	};
+	(witness, claim)
+}
 
-	let evaluation_domain_factory = DefaultEvaluationDomainFactory::<BinaryField8b>::default();
+#[allow(clippy::type_complexity)]
+fn generate_mul_witnesses_claims<const LOG_SIZE: usize, const COLUMN_LEN: usize>(
+	exponent_bit_width: usize,
+) -> (
+	Vec<GeneratorExponentClaim<F>>,
+	Vec<GeneratorExponentWitness<'static, PGenerator, PChallenge>>,
+) {
+	let mut rng = thread_rng();
 
-	let backend = make_portable_backend();
+	let a: [u128; COLUMN_LEN] = array::from_fn(|_| rng.gen::<u128>() % (1 << exponent_bit_width));
+	let b: [u128; COLUMN_LEN] = array::from_fn(|_| rng.gen::<u128>() % (1 << exponent_bit_width));
 
-	let reduced_claims =
-		prove::prove(&witness, &claim, evaluation_domain_factory, transcript, &backend).unwrap();
+	let c: [u128; COLUMN_LEN] = array::from_fn(|i| a[i] * b[i]);
 
-	(claim, witness, reduced_claims)
+	let eval_point_1 = [F::default(); LOG_SIZE].map(|_| <F as Field>::random(&mut rng));
+
+	let eval_point_2 = [F::default(); LOG_SIZE].map(|_| <F as Field>::random(&mut rng));
+
+	let (a_witness, a_claim) =
+		generate_claim_witness::<COLUMN_LEN>(a, exponent_bit_width, None, &eval_point_1);
+	let (b_witness, b_claim) = generate_claim_witness::<COLUMN_LEN>(
+		b,
+		exponent_bit_width,
+		Some(a_witness.exponentiation_result().unwrap()),
+		&eval_point_2,
+	);
+
+	let (c_witness, c_claim) =
+		generate_claim_witness::<COLUMN_LEN>(c, exponent_bit_width * 2, None, &eval_point_2);
+
+	assert_eq!(b_claim.eval, c_claim.eval);
+
+	let witnesses = [c_witness, b_witness, a_witness];
+	let claims = [c_claim, b_claim, a_claim];
+
+	(claims.to_vec(), witnesses.to_vec())
+}
+
+#[allow(clippy::type_complexity)]
+fn generate_mul_witnesses_claims_with_different_log_size() -> (
+	Vec<GeneratorExponentWitness<'static, PGenerator, PChallenge>>,
+	Vec<GeneratorExponentClaim<F>>,
+) {
+	const LOG_SIZE_1: usize = 13usize;
+	const COLUMN_LEN_1: usize = 1usize << LOG_SIZE_1;
+
+	const LOG_SIZE_2: usize = 14usize;
+	const COLUMN_LEN_2: usize = 1usize << LOG_SIZE_2;
+
+	const EXPONENT_BIT_WIDTH: usize = 2usize;
+
+	let (claims_1, witnesses_1) =
+		generate_mul_witnesses_claims::<LOG_SIZE_1, COLUMN_LEN_1>(EXPONENT_BIT_WIDTH);
+
+	let (claims_2, witnesses_2) =
+		generate_mul_witnesses_claims::<LOG_SIZE_2, COLUMN_LEN_2>(EXPONENT_BIT_WIDTH);
+
+	let witnesses = [witnesses_1, witnesses_2].concat();
+
+	let claims = [claims_1, claims_2].concat();
+
+	(witnesses, claims)
 }
 
 #[test]
@@ -114,71 +163,120 @@ where
 fn witness_gen_happens_correctly() {
 	const LOG_SIZE: usize = 13usize;
 	const COLUMN_LEN: usize = 1usize << LOG_SIZE;
+	const EXPONENT_BIT_WIDTH: usize = 8usize;
 
 	let mut rng = thread_rng();
-	let exponent: [u64; COLUMN_LEN] = array::from_fn(|_| rng.gen());
 
-	let witness = generate_witness_for_testing(exponent);
+	let a: [u128; COLUMN_LEN] = array::from_fn(|_| rng.gen::<u8>() as u128);
+	let b: [u128; COLUMN_LEN] = array::from_fn(|_| rng.gen::<u8>() as u128);
+	let c: [u128; COLUMN_LEN] = array::from_fn(|i| a[i] * b[i]);
 
-	let results = &witness.single_bit_output_layers_data[63];
-	for (row_idx, this_row_exponent) in exponent.into_iter().enumerate() {
+	let eval_point = [F::default(); LOG_SIZE].map(|_| <F as Field>::random(&mut rng));
+
+	let (a_witness, _) =
+		generate_claim_witness::<COLUMN_LEN>(a, EXPONENT_BIT_WIDTH, None, &eval_point);
+	let a_exponentiation_result = a_witness.exponentiation_result().unwrap();
+
+	for (row_idx, this_row_exponent) in a.into_iter().enumerate() {
 		assert_eq!(
 			<PGenerator as PackedField>::Scalar::MULTIPLICATIVE_GENERATOR
 				.pow(this_row_exponent as u64),
-			get_packed_slice(results, row_idx)
+			get_packed_slice(a_exponentiation_result, row_idx)
 		);
 	}
+
+	let (b_witness, _) = generate_claim_witness::<COLUMN_LEN>(
+		b,
+		EXPONENT_BIT_WIDTH,
+		Some(a_exponentiation_result),
+		&eval_point,
+	);
+	let b_exponentiation_result = b_witness.exponentiation_result().unwrap();
+
+	for (row_idx, this_row_exponent) in b.into_iter().enumerate() {
+		assert_eq!(
+			get_packed_slice(a_exponentiation_result, row_idx).pow(this_row_exponent as u64),
+			get_packed_slice(b_exponentiation_result, row_idx)
+		);
+	}
+
+	let (c_witness, _) =
+		generate_claim_witness::<COLUMN_LEN>(c, EXPONENT_BIT_WIDTH * 2, None, &eval_point);
+	let c_exponentiation_result = c_witness.exponentiation_result().unwrap();
+
+	assert_eq!(b_exponentiation_result, c_exponentiation_result);
 }
 
 #[test]
 fn prove_reduces_to_correct_claims() {
-	const LOG_SIZE: usize = 13usize;
-	const COLUMN_LEN: usize = 1usize << LOG_SIZE;
-	const EXPONENT_BIT_WIDTH: usize = 64usize;
-
 	let mut transcript = ProverTranscript::<HasherChallenger<Groestl256>>::new();
-	let reduced_claims =
-		generate_witness_and_prove::<LOG_SIZE, COLUMN_LEN, EXPONENT_BIT_WIDTH, _>(&mut transcript);
 
-	let (_, witness, evals_on_bit_columns) = reduced_claims;
+	let (witnesses, claims): (Vec<_>, Vec<_>) =
+		generate_mul_witnesses_claims_with_different_log_size();
 
-	// Check the evaluations of the exponent bit columns
+	let evaluation_domain_factory = DefaultEvaluationDomainFactory::<BinaryField8b>::default();
 
-	for (this_bit_witness, this_claim) in
-		izip!(witness.exponent, evals_on_bit_columns.eval_claims_on_exponent_bit_columns)
+	let backend = make_portable_backend();
+
+	let GeneratorExponentReductionOutput {
+		eval_claims_on_exponent_bit_columns,
+	} = prove::batch_prove(
+		witnesses.clone(),
+		&claims,
+		evaluation_domain_factory,
+		&mut transcript,
+		&backend,
+	)
+	.unwrap();
+
+	for (exponent_bit_number, eval_claims_on_exponent_bit_columns) in
+		eval_claims_on_exponent_bit_columns.iter().enumerate()
 	{
-		let this_bit_query = MultilinearQuery::expand(&this_claim.eval_point);
+		let mut j = 0;
+		for witness in &witnesses {
+			let exponent_len = witness.exponent.len() - 1;
 
-		let claimed_evaluation = this_claim.eval;
+			if exponent_len < exponent_bit_number {
+				continue;
+			}
 
-		let actual_evaluation = this_bit_witness
-			.evaluate(MultilinearQueryRef::new(&this_bit_query))
-			.unwrap();
+			let this_claim = &eval_claims_on_exponent_bit_columns[j];
+			let this_bit_query = MultilinearQuery::expand(&this_claim.eval_point);
+			let claimed_evaluation = this_claim.eval;
 
-		assert_eq!(claimed_evaluation, actual_evaluation);
+			let exponent = if witness.generator.is_some() {
+				witness.exponent[exponent_bit_number].clone()
+			} else {
+				witness.exponent[exponent_len - exponent_bit_number].clone()
+			};
+
+			let actual_evaluation = exponent
+				.evaluate(MultilinearQueryRef::new(&this_bit_query))
+				.unwrap();
+			assert_eq!(claimed_evaluation, actual_evaluation);
+			j += 1;
+		}
 	}
 }
 
 #[test]
 fn good_proof_verifies() {
-	const LOG_SIZE: usize = 13usize;
-	const COLUMN_LEN: usize = 1usize << LOG_SIZE;
-	const EXPONENT_BIT_WIDTH: usize = 64usize;
-
 	let mut transcript = ProverTranscript::<HasherChallenger<Groestl256>>::new();
-	let reduced_claims =
-		generate_witness_and_prove::<LOG_SIZE, COLUMN_LEN, EXPONENT_BIT_WIDTH, _>(&mut transcript);
 
-	let (claim, _, _) = reduced_claims;
+	let (witnesses, claims): (Vec<_>, Vec<_>) =
+		generate_mul_witnesses_claims_with_different_log_size();
+
+	let evaluation_domain_factory = DefaultEvaluationDomainFactory::<BinaryField8b>::default();
+
+	let backend = make_portable_backend();
+
+	prove::batch_prove(witnesses, &claims, evaluation_domain_factory, &mut transcript, &backend)
+		.unwrap();
 
 	let mut verifier_transcript = transcript.into_verifier();
 
-	let _reduced_claims = verify::verify::<FGenerator, _, _, EXPONENT_BIT_WIDTH>(
-		&claim,
-		&mut verifier_transcript,
-		LOG_SIZE,
-	)
-	.unwrap();
+	let _reduced_claims =
+		verify::batch_verify::<FGenerator, _, _>(claims, &mut verifier_transcript).unwrap();
 
 	verifier_transcript.finalize().unwrap()
 }
--- crates/core/src/protocols/gkr_int_mul/generator_exponent/witness.rs
+++ crates/core/src/protocols/gkr_int_mul/generator_exponent/witness.rs
@@ -1,27 +1,27 @@
 // Copyright 2024-2025 Irreducible Inc.
 
-use std::{array, cmp::min, slice};
+use std::{cmp::min, slice};
 
 use binius_field::{
 	ext_base_op_par, BinaryField, BinaryField1b, ExtensionField, PackedExtension, PackedField,
 };
+use binius_math::{MLEEmbeddingAdapter, MultilinearExtension};
 use binius_maybe_rayon::{
 	prelude::{IndexedParallelIterator, ParallelIterator},
 	slice::ParallelSliceMut,
 };
 use bytemuck::zeroed_vec;
 
-use crate::{protocols::gkr_gpa::Error, witness::MultilinearWitness};
-pub struct GeneratorExponentWitness<
-	'a,
-	PBits: PackedField,
-	PGenerator: PackedField,
-	PChallenge: PackedField,
-	const EXPONENT_BIT_WIDTH: usize,
-> {
-	pub exponent: [MultilinearWitness<'a, PChallenge>; EXPONENT_BIT_WIDTH],
-	pub exponent_data: [Vec<PBits>; EXPONENT_BIT_WIDTH],
-	pub single_bit_output_layers_data: [Vec<PGenerator>; EXPONENT_BIT_WIDTH],
+use crate::{
+	protocols::{gkr_gpa::Error, sumcheck::equal_n_vars_check},
+	witness::MultilinearWitness,
+};
+
+#[derive(Clone)]
+pub struct GeneratorExponentWitness<'a, PGenerator: PackedField, P: PackedField> {
+	pub exponent: Vec<MultilinearWitness<'a, P>>,
+	pub single_bit_output_layers_data: Vec<Vec<PGenerator>>,
+	pub generator: Option<MultilinearWitness<'a, P>>,
 }
 
 fn copy_witness_into_vec<P, PE>(poly: &MultilinearWitness<PE>) -> Vec<P>
@@ -62,7 +62,7 @@ where
 
 fn evaluate_single_bit_output_packed<PBits, PGenerator>(
 	exponent_bit: &[PBits],
-	generator_power_constant: PGenerator::Scalar,
+	generator: Generator<PGenerator>,
 	previous_single_bit_output: &[PGenerator],
 ) -> Vec<PGenerator>
 where
@@ -77,22 +77,29 @@ where
 
 	let mut result = previous_single_bit_output.to_vec();
 
-	let packed_generator_power = PGenerator::broadcast(generator_power_constant);
+	let _ = ext_base_op_par(&mut result, exponent_bit, |i, prev_out, exp_bit_broadcasted| {
+		let (generator, prev_out) = match &generator {
+			Generator::Static(g) => (*g, prev_out),
+			Generator::Dynamic(g) => (g[i], prev_out * prev_out),
+		};
 
-	let _ = ext_base_op_par(&mut result, exponent_bit, |_, prev_out, exp_bit_broadcasted| {
 		prev_out
 			* (PGenerator::cast_ext(
-				PGenerator::cast_base(packed_generator_power - PGenerator::one())
-					* exp_bit_broadcasted,
+				PGenerator::cast_base(generator - PGenerator::one()) * exp_bit_broadcasted,
 			) + PGenerator::one())
 	});
 
 	result
 }
 
+enum Generator<'a, PGenerator: PackedField> {
+	Static(PGenerator),
+	Dynamic(&'a [PGenerator]),
+}
+
 fn evaluate_first_layer_output_packed<PBits, PGenerator>(
 	exponent_bit: &[PBits],
-	generator_power_constant: PGenerator::Scalar,
+	generator: Generator<PGenerator>,
 ) -> Vec<PGenerator>
 where
 	PBits: PackedField<Scalar = BinaryField1b>,
@@ -101,56 +108,110 @@ where
 {
 	let mut result = vec![PGenerator::zero(); exponent_bit.len() * PGenerator::Scalar::DEGREE];
 
-	let packed_generator_power = PGenerator::broadcast(generator_power_constant);
+	let _ = ext_base_op_par(&mut result, exponent_bit, |i, _, exp_bit_broadcasted| {
+		let generator = match &generator {
+			Generator::Static(g) => *g,
+			Generator::Dynamic(g) => g[i],
+		};
 
-	let _ = ext_base_op_par(&mut result, exponent_bit, |_, _, exp_bit_broadcasted| {
 		PGenerator::cast_ext(
-			PGenerator::cast_base(packed_generator_power - PGenerator::one()) * exp_bit_broadcasted,
+			PGenerator::cast_base(generator - PGenerator::one()) * exp_bit_broadcasted,
 		) + PGenerator::one()
 	});
 
 	result
 }
 
-impl<'a, PBits, PGenerator, PChallenge, const EXPONENT_BIT_WIDTH: usize>
-	GeneratorExponentWitness<'a, PBits, PGenerator, PChallenge, EXPONENT_BIT_WIDTH>
+impl<'a, PGenerator, P> GeneratorExponentWitness<'a, PGenerator, P>
 where
-	PBits: PackedField<Scalar = BinaryField1b>,
-	PGenerator: PackedExtension<PBits::Scalar, PackedSubfield = PBits>,
+	PGenerator: PackedField,
 	PGenerator::Scalar: BinaryField,
-	PChallenge: PackedExtension<PBits::Scalar, PackedSubfield = PBits>,
-	PChallenge::Scalar: BinaryField,
+	P: PackedExtension<PGenerator::Scalar, PackedSubfield = PGenerator>,
+	P::Scalar: BinaryField + ExtensionField<PGenerator::Scalar>,
 {
-	pub fn new(
-		exponent: [MultilinearWitness<'a, PChallenge>; EXPONENT_BIT_WIDTH],
-	) -> Result<Self, Error> {
-		let num_rows = 1 << exponent[0].n_vars();
+	pub fn new_with_static_generator<PBits>(
+		exponent: Vec<MultilinearWitness<'a, P>>,
+	) -> Result<Self, Error>
+	where
+		PBits: PackedField<Scalar = BinaryField1b>,
+		PGenerator: PackedExtension<PBits::Scalar, PackedSubfield = PBits>,
+		P: PackedExtension<PBits::Scalar, PackedSubfield = PBits>,
+	{
+		equal_n_vars_check(&exponent)?;
 
-		let exponent_data = array::from_fn(|i| copy_witness_into_vec(&exponent[i]));
+		let exponent_bit_width = exponent.len();
 
-		let mut single_bit_output_layers_data =
-			array::from_fn(|_| vec![PGenerator::zero(); num_rows / PGenerator::WIDTH]);
+		let mut single_bit_output_layers_data = vec![Vec::new(); exponent_bit_width];
+
+		let mut packed_generator_power_constant =
+			PGenerator::broadcast(PGenerator::Scalar::MULTIPLICATIVE_GENERATOR);
 
 		single_bit_output_layers_data[0] = evaluate_first_layer_output_packed::<PBits, PGenerator>(
-			&exponent_data[0],
-			PGenerator::Scalar::MULTIPLICATIVE_GENERATOR,
+			&copy_witness_into_vec(&exponent[0]),
+			Generator::Static(packed_generator_power_constant),
 		);
 
-		let mut generator_power_constant = PGenerator::Scalar::MULTIPLICATIVE_GENERATOR.square();
+		for layer_idx_from_left in 1..exponent_bit_width {
+			packed_generator_power_constant = packed_generator_power_constant.square();
 
-		for layer_idx_from_left in 1..EXPONENT_BIT_WIDTH {
 			single_bit_output_layers_data[layer_idx_from_left] = evaluate_single_bit_output_packed(
-				&exponent_data[layer_idx_from_left],
-				generator_power_constant,
+				&copy_witness_into_vec(&exponent[layer_idx_from_left]),
+				Generator::Static(packed_generator_power_constant),
 				&single_bit_output_layers_data[layer_idx_from_left - 1],
 			);
-			generator_power_constant = generator_power_constant.square();
 		}
 
 		Ok(Self {
 			exponent,
-			exponent_data,
 			single_bit_output_layers_data,
+			generator: None,
 		})
+	}
+
+	pub fn new_with_dynamic_generator<PBits>(
+		exponent: Vec<MultilinearWitness<'a, P>>,
+		generator_evals: &[PGenerator],
+	) -> Result<Self, Error>
+	where
+		PBits: PackedField<Scalar = BinaryField1b>,
+		PGenerator: PackedExtension<PBits::Scalar, PackedSubfield = PBits>,
+		P: PackedExtension<PBits::Scalar, PackedSubfield = PBits>,
+	{
+		equal_n_vars_check(&exponent)?;
+
+		let exponent_bit_width = exponent.len();
+
+		let mut single_bit_output_layers_data = vec![Vec::new(); exponent_bit_width];
+
+		single_bit_output_layers_data[0] = evaluate_first_layer_output_packed::<PBits, PGenerator>(
+			&copy_witness_into_vec(&exponent[exponent_bit_width - 1]),
+			Generator::Dynamic(generator_evals),
+		);
+
+		for layer_idx_from_left in 1..exponent_bit_width {
+			single_bit_output_layers_data[layer_idx_from_left] = evaluate_single_bit_output_packed(
+				&copy_witness_into_vec(&exponent[exponent_bit_width - layer_idx_from_left - 1]),
+				Generator::Dynamic(generator_evals),
+				&single_bit_output_layers_data[layer_idx_from_left - 1],
+			);
+		}
+
+		let n_vars = exponent[0].n_vars();
+
+		let generator = MultilinearExtension::new(n_vars, generator_evals.to_vec())
+			.map(MLEEmbeddingAdapter::<PGenerator, P>::from)?
+			.upcast_arc_dyn();
+
+		Ok(Self {
+			exponent,
+			single_bit_output_layers_data,
+			generator: Some(generator),
+		})
+	}
+
+	pub fn exponentiation_result(&self) -> Option<&[PGenerator]> {
+		self.single_bit_output_layers_data
+			.last()
+			.map(|value| value.as_slice())
 	}
 }
