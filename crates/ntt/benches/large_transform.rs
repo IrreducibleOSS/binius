0ebd08e3c8d941f1e976edb66c6c9694e00a1a3c
AnexJ <ajohnson@ulvetanna.io>
2025-03-03 11:39:59 -0500

    Added byte sliced additive ntt benchmarks as well as AES32b

--- crates/ntt/benches/large_transform.rs
+++ crates/ntt/benches/large_transform.rs
@@ -3,17 +3,17 @@
 use std::iter::repeat_with;
 
 use binius_field::{
-	arch::OptimalUnderlier, as_packed_field::PackedType, BinaryField32b, PackedField,
+	arch::OptimalUnderlier, as_packed_field::PackedType, AESTowerField32b, BinaryField32b,
+	ByteSlicedAES16x32b, PackedField, TowerField,
 };
 use binius_ntt::{AdditiveNTT, SingleThreadedNTT};
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use rand::thread_rng;
 
-fn bench_large_transform(c: &mut Criterion) {
-	type U = OptimalUnderlier;
-	type F = BinaryField32b;
-	type P = PackedType<U, F>;
-
+fn bench_large_transform<F: TowerField, P: PackedField<Scalar = F>>(
+	c: &mut Criterion,
+	field: &str,
+) {
 	let mut group = c.benchmark_group("slow/transform");
 	for log_n in std::iter::once(17) {
 		for log_batch_size in [4, 6] {
@@ -23,7 +23,7 @@ fn bench_large_transform(c: &mut Criterion) {
 				.take(data_len)
 				.collect::<Vec<_>>();
 
-			let params = format!("field=BinaryField32b/log_n={log_n}/log_b={log_batch_size}");
+			let params = format!("{field}/log_n={log_n}/log_b={log_batch_size}");
 			group.throughput(Throughput::Bytes((data_len * size_of::<P>()) as u64));
 
 			let ntt = SingleThreadedNTT::<F>::new(log_n)
@@ -44,9 +44,29 @@ fn bench_large_transform(c: &mut Criterion) {
 	}
 }
 
+// We are ignoring the transposition associated with byte slicing
+fn bench_byte_sliced(c: &mut Criterion) {
+	type F = AESTowerField32b;
+	type PByteSliced = ByteSlicedAES16x32b;
+
+	bench_large_transform::<F, PByteSliced>(c, "bytesliced=ByteSlicedAES16x32b")
+}
+
+fn bench_packed32(c: &mut Criterion) {
+	bench_large_transform::<BinaryField32b, PackedType<OptimalUnderlier, BinaryField32b>>(
+		c,
+		"field=BinaryField32b",
+	);
+
+	bench_large_transform::<AESTowerField32b, PackedType<OptimalUnderlier, AESTowerField32b>>(
+		c,
+		"field=AESTowerField32b",
+	);
+}
+
 criterion_group! {
 	name = large_transform;
 	config = Criterion::default().sample_size(10);
-	targets = bench_large_transform
+	targets = bench_packed32, bench_byte_sliced
 }
 criterion_main!(large_transform);
