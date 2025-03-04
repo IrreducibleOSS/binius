8d990ba04e5e7544bee09f540671f081d808b88f
Thomas Coratger <thomas.coratger@gmail.com>
2025-03-04 21:44:16 +0100

    clean up

--- crates/field/src/byte_iteration.rs
+++ crates/field/src/byte_iteration.rs
@@ -303,56 +303,26 @@ pub fn create_partial_sums_lookup_tables<P: PackedField>(
 	let len = values.len();
 	assert!(len % 8 == 0);
 
-	let mut result = Vec::with_capacity(len * 256 / 8);
-	for chunk_i in 0..len / 8 {
-		let offset = chunk_i * 8;
-		for i in 0..256 {
-			let mut sum = P::zero();
-			for j in 0..8 {
-				if i & (1 << j) != 0 {
-					sum += values.get(offset + j);
+	let mut result = Vec::with_capacity(len * 32); // 256 / 8 = 32 per chunk
+
+	for chunk_start in (0..len).step_by(8) {
+		let mut sums = [P::zero(); 256];
+
+		for j in 0..8 {
+			let value = values.get(chunk_start + j);
+			let mask = 1 << j;
+			for i in (mask..256).step_by(mask * 2) {
+				for k in 0..mask {
+					sums[i + k] += value;
 				}
 			}
-			result.push(sum);
 		}
+
+		result.extend_from_slice(&sums);
 	}
 
 	result
 }
-
-// /// Create a lookup table for partial sums of 8 consequent elements with coefficients corresponding to bits in a byte.
-// /// The lookup table has the following structure:
-// /// [
-// ///     partial_sum_chunk_0_7_byte_0, partial_sum_chunk_0_7_byte_1, ..., partial_sum_chunk_0_7_byte_255,
-// ///     partial_sum_chunk_8_15_byte_0, partial_sum_chunk_8_15_byte_1, ..., partial_sum_chunk_8_15_byte_255,
-// ///    ...
-// /// ]
-// pub fn create_partial_sums_lookup_tables<P: PackedField>(
-// 	values: impl ScalarsCollection<P>,
-// ) -> Vec<P> {
-// 	let len = values.len();
-// 	assert!(len % 8 == 0);
-
-// 	let mut result = Vec::with_capacity(len * 32); // 256 / 8 = 32 per chunk
-
-// 	for chunk_start in (0..len).step_by(8) {
-// 		let mut sums = [P::zero(); 256];
-
-// 		for j in 0..8 {
-// 			let value = values.get(chunk_start + j);
-// 			let mask = 1 << j;
-// 			for i in (mask..256).step_by(mask * 2) {
-// 				for k in 0..mask {
-// 					sums[i + k] += value;
-// 				}
-// 			}
-// 		}
-
-// 		result.extend_from_slice(&sums);
-// 	}
-
-// 	result
-// }
 
 #[cfg(test)]
 mod tests {
