5c355b33056188731749b1dc49feb700682ae5bf
Jim Posen <jimpo@ulvetanna.io>
2025-03-11 23:34:46 +0100

    Reorder comment line

--- crates/m3/src/lib.rs
+++ crates/m3/src/lib.rs
@@ -1,5 +1,6 @@
-#![feature(array_try_map)]
 // Copyright 2025 Irreducible Inc.
+
+#![feature(array_try_map)]
 
 //! A library for building Binius constraint systems and instances using M3 arithmetization.
 //!
