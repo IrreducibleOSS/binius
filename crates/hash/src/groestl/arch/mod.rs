6fd9b679fe9ed37351d8777d4ae26fb89c182b98
Jakub Zalewski <jakzale@gmail.com>
2025-03-21 17:04:32 +0100

    Gate groestl_avx512 on nightly_features

--- crates/hash/src/groestl/arch/mod.rs
+++ crates/hash/src/groestl/arch/mod.rs
@@ -6,7 +6,7 @@ use cfg_if::cfg_if;
 // extensions, otherwise defaults to the portable implementation which was found to be fast in most
 // machines
 cfg_if! {
-	if #[cfg(all(target_arch = "x86_64",target_feature = "avx512bw",target_feature = "avx512vbmi",target_feature = "avx512f",target_feature = "gfni",))] {
+	if #[cfg(all(feature = "nightly_features", target_arch = "x86_64",target_feature = "avx512bw",target_feature = "avx512vbmi",target_feature = "avx512f",target_feature = "gfni",))] {
 		mod groestl_avx512;
 		pub use groestl_avx512::GroestlShortImpl;
 	} else {
