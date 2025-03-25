18c2da2d43e358d2f69e1a702dec255cb8a73914
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-03-25 14:46:07 +0300

    chore: refactor file search and improve code readability

--- scripts/check_copyright_notice.sh
+++ scripts/check_copyright_notice.sh
@@ -4,13 +4,15 @@ set -e
 check_copyright_notices() {
     exitcode=0
     for file in $1; do
-        if (head -n1 "$file" | grep -q "// Copyright .* Ulvetanna Inc."); then
+        first_line=$(head -n1 "$file")
+        
+        if echo "$first_line" | grep -q "// Copyright .* Ulvetanna Inc."; then
             echo "$file: ERROR - Copyright notice is using Ulvetanna instead of Irreducible"
             exitcode=1
-        elif ! (head -n1 "$file" | grep -q "// Copyright "); then
+        elif ! echo "$first_line" | grep -q "// Copyright "; then
             echo "$file: ERROR - Copyright notice missing on first line"
             exitcode=1
-        elif ! (head -n1 "$file" | grep -q "2025"); then
+        elif ! echo "$first_line" | grep -q "2025"; then
             echo "$file: ERROR - Copyright notice does not contain the year 2025"
             exitcode=1
         fi
@@ -18,7 +20,4 @@ check_copyright_notices() {
     exit $exitcode
 }
 
-check_copyright_notices "$(
-    find crates -type f -name '*.rs';
-    find examples -type f -name '*.rs'
-)"
+check_copyright_notices "$(find crates examples -type f -name '*.rs')"
