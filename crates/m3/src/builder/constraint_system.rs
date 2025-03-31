47c8c3db96bbe61ecd4ce189cb9b4001cc092491
Alonso Gonzalez <alonso.gon@gmail.com>
2025-03-31 11:40:03 +0200

    Fmt

--- crates/m3/src/builder/constraint_system.rs
+++ crates/m3/src/builder/constraint_system.rs
@@ -163,11 +163,7 @@ impl<F: TowerField> ConstraintSystem<F> {
 				.zip(&statement.table_sizes)
 				.map(|(table, &table_size)| {
 					if table_size > 0 {
-						Some(TableWitnessIndex::new(
-							allocator,
-							table,
-							table_size,
-						))
+						Some(TableWitnessIndex::new(allocator, table, table_size))
 					} else {
 						None
 					}
