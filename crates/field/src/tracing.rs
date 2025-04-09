46f2e5f86b20f06b051b2a9aa4043d43551eaa38
Dmytro Gordon <dgordon@irreducible.io>
2025-04-09 18:49:58 +0300

    Deduplicate events when tracing multiplications

--- crates/field/src/tracing.rs
+++ crates/field/src/tracing.rs
@@ -1,19 +1,59 @@
 // Copyright 2024-2025 Irreducible Inc.
+use cfg_if::cfg_if;
 
-/// Trace multiplication event
-macro_rules! trace_multiplication {
-    ($name: ty) => {
-        #[cfg(feature = "trace_multiplications")]
-        {
-            tracing::event!(name: "mul", tracing::Level::TRACE, {lhs = stringify!($name), rhs = stringify!($name)});
-        }
-    };
-    ($lhs: ty, $rhs: ty) => {
-        #[cfg(feature = "trace_multiplications")]
-        {
-            tracing::event!(name: "mul", tracing::Level::TRACE, {lhs = stringify!($lhs), rhs = stringify!($rhs)});
-        }
-    };
+cfg_if! {
+	if #[cfg(feature = "trace_multiplications")] {
+		use std::cell::Cell;
+
+		thread_local! {
+			static IS_IN_MULT_FUNCTION: Cell<bool> = Cell::new(false);
+		}
+
+		/// This guard is used to remove the duplicated items when one field multiplication
+		/// re-uses several other ones.
+		pub(crate) struct TraceGuard(bool);
+
+		impl TraceGuard {
+			pub fn new(lhs: &'static str, rhs: &'static str) -> Self {
+				let val = IS_IN_MULT_FUNCTION.with(|v| {
+					if !v.get() {
+						v.set(true);
+						tracing::event!(name: "mul", tracing::Level::TRACE, {lhs, rhs});
+
+						true
+					} else {
+						false
+					}
+				});
+				Self(val)
+			}
+		}
+
+		impl Drop for TraceGuard {
+			fn drop(&mut self) {
+				if self.0 {
+					IS_IN_MULT_FUNCTION.with(|v| {
+						v.set(false);
+					});
+				};
+			}
+		}
+
+		macro_rules! trace_multiplication {
+			($name: ty) => {
+				let _guard = $crate::tracing::TraceGuard::new(stringify!($name), stringify!($name));
+			};
+			($lhs: ty, $rhs: ty) => {
+				let _guard = $crate::tracing::TraceGuard::new(stringify!($lhs), stringify!($rhs));
+			};
+		}
+
+	} else {
+		macro_rules! trace_multiplication {
+			($name: ty) => {};
+			($lhs: ty, $rhs: ty) => {};
+		}
+	}
 }
 
 pub(crate) use trace_multiplication;
