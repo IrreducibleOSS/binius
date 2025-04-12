1f9d6049a8da18dd961f52eb7c6d6d936f971002
Alex Euler <0xalexeuler@gmail.com>
2025-04-12 13:11:54 +0300

    added tests for deserialize_bytes

--- crates/macros/src/deserialize_bytes.rs
+++ crates/macros/src/deserialize_bytes.rs
@@ -32,3 +32,63 @@ pub fn get_generics<'attr, 'gen>(
 	let generics_split = GenericsSplit::new(generics, container_attributes.eval_generics);
 	Ok(generics_split)
 }
+
+#[cfg(test)]
+mod tests {
+	use ::quote::quote;
+	use proc_macro2::TokenStream;
+	use syn::{parse_quote, DeriveInput};
+
+	use super::*;
+
+	#[test]
+	fn test_get_generics() {
+		struct Case {
+			struct_def: TokenStream,
+			expected_impl_def: TokenStream,
+		}
+		let cases = vec![
+			Case {
+				struct_def: quote! {
+					#[derive(DeserializeBytes)]
+					#[deserialize_bytes(eval_generics(F = BinaryField128b))]
+					pub struct MultilinearOracleSet<F: TowerField> {
+						oracles: Vec<MultilinearPolyOracle<F>>
+					}
+				},
+				expected_impl_def: quote! {
+					impl MultilinearOracleSet<BinaryField128b>
+				},
+			},
+			Case {
+				struct_def: quote! {
+					#[derive(DeserializeBytes)]
+					#[deserialize_bytes(eval_generics(F = BinaryField128b))]
+					pub struct FieldPair<F: TowerField, G: TowerField + DeserializeBytes>
+					where G: Debug
+					{
+						x: F,
+						y: G,
+					}
+				},
+				expected_impl_def: quote! {
+					impl <G: TowerField + DeserializeBytes> FieldPair<BinaryField128b, G>
+					where G: Debug
+				},
+			},
+		];
+		for case in cases {
+			let input = syn::parse2::<DeriveInput>(case.struct_def).unwrap();
+			let GenericsSplit {
+				impl_generics,
+				type_generics,
+				where_clause,
+			} = get_generics(&input.attrs, &input.generics).unwrap();
+			let name = input.ident;
+			let impl_def = quote! {
+				impl #impl_generics #name #type_generics #where_clause
+			};
+			assert_eq!(impl_def.to_string(), case.expected_impl_def.to_string());
+		}
+	}
+}
