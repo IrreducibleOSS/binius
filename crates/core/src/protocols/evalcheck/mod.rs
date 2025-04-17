3f106f0512b3c77714f93c2555e479dfcc3d1acd
AnexJ <ajohnson@ulvetanna.io>
2025-04-17 10:33:52 -0400

    added module level documentation

--- crates/core/src/protocols/evalcheck/mod.rs
+++ crates/core/src/protocols/evalcheck/mod.rs
@@ -2,8 +2,13 @@
 
 //! The multivariate evalcheck polynomial protocol.
 //!
-//! Being largely intended for claims coming from sumcheck/zerocheck invocations, this module only supports
-//! multilinear composites and not general multivariate polynomials.
+//! This protocol can be used to verify a list of multilinear composite claims in the [`MultilinearOracleSet`].
+//! For each claim, if they contain a subclaim, we recursively prove the subclaim. Otherwise,
+//! we evaluate the claim if they are virtual, further reduced to sumcheck constraints for
+//! [`MultilinearPolyVariant::Shifted`], [`MultilinearPolyVariant::Packed`], or [`MultilinearPolyVariant::Composite`].
+//! All the committed polynomials are collected and should be handled by the[`ring_switch`] module.
+//! [`greedy_evalcheck`] shows how the protocol is used in a round-by-round manner.
+//! See [`EvalcheckProver::prove`] for more details.
 
 mod error;
 #[allow(clippy::module_inception)]
