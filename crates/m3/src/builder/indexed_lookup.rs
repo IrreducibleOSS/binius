544e3648fde4ff47a33871a6610fc952aa2de158
Jim Posen <jimpo@irreducible.com>
2025-05-01 21:26:28 +0200

    Progress

--- crates/m3/src/builder/indexed_lookup.rs
+++ crates/m3/src/builder/indexed_lookup.rs
@@ -2,26 +2,41 @@
 
 use std::sync::atomic::AtomicU32;
 
-use binius_core::constraint_system::channel::{Boundary, ChannelId};
+use binius_core::constraint_system::channel::{Boundary, ChannelId, FlushDirection};
+use binius_field::{PackedField, TowerField};
 
-use crate::builder::{ConstraintSystem, WitnessIndex};
+use crate::builder::{ConstraintSystem, WitnessIndex, B128};
 
-pub trait LookupTable {
-	type Entry: Clone;
-
+pub trait IndexedLookup<F: TowerField> {
 	/// Binary logarithm of the number of table entries.
 	///
 	/// This must be at most 32.
 	fn log_size(&self) -> usize;
 
-	fn entry_to_index(&self, entry: Self::Entry) -> u32;
+	fn entry_to_index(&self, entry: &[F]) -> u32;
 }
 
-pub fn tally<U, F>(
-	cs: &ConstraintSystem<F>,
-	witness: &WitnessIndex<U, F>,
-	statement: &[Boundary<F>],
+pub fn tally<P>(
+	cs: &ConstraintSystem<B128>,
+	witness: &mut WitnessIndex<P>,
+	boundaries: &[Boundary<B128>],
 	chan: ChannelId,
-) -> Vec<AtomicU32> {
-	// Identify all tables with pulls from the channel.
+	indexed_lookup: &impl IndexedLookup<B128>,
+) -> Vec<u32>
+where
+	P: PackedField<Scalar = B128>,
+{
+	for table in cs.tables {
+		for partition in table.partitions.values() {
+			for flush in partition.flushes {
+				if flush.channel_id == chan && flush.direction == FlushDirection::Pull {
+					if let Some(table_index) = witness.get_table(table.id()) {
+						let segment = table_index.full_segment();
+						let cols = flush.column_indices.iter().map(|&col_index| {});
+					}
+				}
+			}
+		}
+	}
+	Vec::new()
 }
