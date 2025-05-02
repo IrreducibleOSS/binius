98d87e33d31e5e8d59f18d1a15c216b513430602
Aliaksei Dziadziuk <adziadziuk@irreducible.com>
2025-05-02 15:09:47 +0200

    update docs

--- crates/m3/src/builder/channel.rs
+++ crates/m3/src/builder/channel.rs
@@ -13,9 +13,9 @@ pub struct Flush {
 	pub direction: FlushDirection,
 	/// The number of times the values are flushed to the channel.
 	pub multiplicity: u32,
-	/// An optional reference to a column to select which values to flush.
+	/// An optional reference to a columns to select which values to flush.
 	///
-	/// The referenced selector column must hold 1-bit values.
+	/// The referenced selector columns must hold 1-bit values.
 	pub selectors: Vec<ColumnIndex>,
 }
 
@@ -24,10 +24,10 @@ pub struct Flush {
 pub struct FlushOpts {
 	/// The number of times the values are flushed to the channel.
 	pub multiplicity: u32,
-	/// An optional reference to a column to select which values to flush.
+	/// An optional reference to a columns to select which values to flush.
 	///
-	/// The referenced selector column must hold 1-bit values and contain only zeros after the
-	/// index that is the height of the table. If the selector is `None`, all values up to the
+	/// The referenced selector columns must hold 1-bit values and contain only zeros after the
+	/// index that is the height of the table. If the selectors is empty, all values up to the
 	/// table height are flushed.
 	pub selectors: Vec<Col<B1>>,
 }
