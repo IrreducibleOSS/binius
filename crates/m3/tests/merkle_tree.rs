27f5a84b84391fcec30c2848b6b010aeb18835ca
HiddenAndBound <ritwiksen9090@gmail.com>
2025-05-14 16:40:25 +0530

    events and channel defined, implementing trace model

--- crates/m3/tests/merkle_tree.rs
+++ crates/m3/tests/merkle_tree.rs
@@ -2,26 +2,79 @@
 /// compression function.
 
 mod model {
-	use binius_m3::builder::{Col, B1, B32, B8};
+	use binius_core::constraint_system::channel::ChannelId;
+	use binius_hash::{
+		groestl::{Groestl256, Groestl256ByteCompression},
+		PseudoCompressionFunction,
+	};
+	use binius_m3::{
+		builder::{Col, ConstraintSystem, B1, B32, B8},
+		emulate::Channel,
+	};
+	use digest::{generic_array::GenericArray, typenum::Gr};
 
-	pub struct MerkleTree {
-        // The root the the leafs belong to
-		root_ids: Col<B8>,
+	// Signature of the Nodes channel: (Root ID, Data, Depth, Index)
+	type NodeFlush = (u8, [u8; 32], u32, u32);
+	// Signature of the Roots channel: (Root ID, Root digest)
+	type RootFlush = (u8, [u8; 32]);
+	pub struct MerkleTreeChannels {
+		nodes: Channel<NodeFlush>,
+		roots: Channel<RootFlush>,
+	}
 
-        // The hash of [ left || right ]
-		parent_data: Col<B8, 64>,
+	impl MerkleTreeChannels {
+		pub fn new() -> Self {
+			Self {
+				nodes: Channel::default(),
+				roots: Channel::default(),
+			}
+		}
+	}
+	pub struct MerkleTreeToken {
+		pub left: [u8; 32],
+		pub right: [u8; 32],
+		pub parent: [u8; 32],
+		pub depth: u32,
+		pub index: u32,
+		pub flush_left: bool,
+		pub flush_right: bool,
+	}
 
-        // Data contained in the left and right children nodes of the parent node
-		left_data: Col<B8, 64>,
-		right_data: Col<B8, 64>,
+	impl MerkleTreeToken {
+		pub fn new(
+			left: [u8; 32],
+			right: [u8; 32],
+			parent: [u8; 32],
+			depth: u32,
+			index: u32,
+			flush_left: bool,
+			flush_right: bool,
+		) -> Self {
+			Self {
+				left,
+				right,
+				parent,
+				depth,
+				index,
+				flush_left,
+				flush_right,
+			}
+		}
+	}
 
-        // Depth of the parent node
-		parent_depth: Col<B8, 64>,
-        // The index of the parent node
-		parent_index: Col<B32>,
+	pub struct MerkleTreeTrace {
+		pub nodes: Vec<MerkleTreeToken>,
+	}
 
-        // Whether to pull the left or right child
-		pull_left: Col<B1>,
-		pull_right: Col<B1>,
+	impl MerkleTreeToken {
+		pub fn fire(
+			&self,
+			node_channel: &mut Channel<NodeFlush>,
+			root_channel: &mut Channel<RootFlush>,
+		) {
+			let concatenation = [self.left.into(), self.right.into()];
+			let compression = Groestl256ByteCompression;
+			compression.compress(concatenation);
+		}
 	}
 }
