b02290210408f92bee92d8bf4f4d59ad0994dc74
jsk11235 <kirmayerjacob@gmail.com>
2025-05-16 18:53:20 +0200

    temporary debugging tools added

--- crates/hash/src/groestl/arch/mod.rs
+++ crates/hash/src/groestl/arch/mod.rs
@@ -5,13 +5,14 @@ use cfg_if::cfg_if;
 // We will choose the AVX512 Implementation of Grøstl if our machine supports the various AVX512
 // extensions, otherwise defaults to the portable implementation which was found to be fast in most
 // machines
-mod groestl_multi_avx2;
-pub use groestl_multi_avx2::Groestl256Multi;
+
 
 cfg_if! {
 	if #[cfg(all(feature = "nightly_features", target_arch = "x86_64",target_feature = "avx512bw",target_feature = "avx512vbmi",target_feature = "avx512f",target_feature = "gfni",))] {
 		mod groestl_avx512;
 		pub use groestl_avx512::GroestlShortImpl;
+		mod groestl_multi_avx2;
+		pub use groestl_multi_avx2::Groestl256Multi;
 	} else {
 		mod portable;
 		pub use portable::GroestlShortImpl;
