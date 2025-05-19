784a667291a5f74f47a849e496c335ce9436f3a6
jsk11235 <kirmayerjacob@gmail.com>
2025-05-19 17:54:02 +0200

    clean

--- crates/hash/src/groestl/arch/groestl_multi_avx2.rs
+++ crates/hash/src/groestl/arch/groestl_multi_avx2.rs
@@ -3,21 +3,21 @@ pub type State = [__m256i; 8];
 const ROUNDS_PER_PERMUTATION: usize = 10;
 const NUM_PARALLEL_SUBSTATES: usize = 4;
 
-use crate::{groestl::Groestl256, multi_digest::MultiDigest};
-use std::array;
-use std::fmt::Write;
 use std::mem::MaybeUninit;
 
+use crate::{groestl::Groestl256, multi_digest::MultiDigest};
+
 const BYTESLICE_PERMUTATION_ARRAY: [u8; 32] = [
-    0, 8, 16, 24, 1, 9, 17, 25, 2, 10, 18, 26, 3, 11, 19, 27, 4, 12, 20, 28, 5, 13, 21, 29, 6, 14,
-    22, 30, 7, 15, 23, 31,
+	0, 8, 16, 24, 1, 9, 17, 25, 2, 10, 18, 26, 3, 11, 19, 27, 4, 12, 20, 28, 5, 13, 21, 29, 6, 14,
+	22, 30, 7, 15, 23, 31,
 ];
 
 const UNBYTESLICE_PERMUTATION_ARRAY: [u8; 32] = [
-    0, 8, 16, 24, 1, 9, 17, 25, 4, 12, 20, 28, 5, 13, 21, 29, 2, 10, 18, 26, 3, 11, 19, 27, 6, 14,
-    22, 30, 7, 15, 23, 31,
+	0, 8, 16, 24, 1, 9, 17, 25, 4, 12, 20, 28, 5, 13, 21, 29, 2, 10, 18, 26, 3, 11, 19, 27, 6, 14,
+	22, 30, 7, 15, 23, 31,
 ];
 
+// These getters/setters are still prototypes
 #[inline]
 fn set_substates_par(substate_vals: [&[u8]; NUM_PARALLEL_SUBSTATES]) -> State {
 	let mut new_state = [unsafe { _mm256_setzero_si256() }; 8];
@@ -78,12 +78,13 @@ fn set_substates_par(substate_vals: [&[u8]; NUM_PARALLEL_SUBSTATES]) -> State {
 		}
 	}
 
-    // swaps because the SIMD instructions operate on the 128-bit lanes as opposed to the whole 256-bit value
+	// swaps because the SIMD instructions operate on the 128-bit lanes as opposed to the whole
+	// 256-bit value
 
-    new_state.swap(1,2);
-    new_state.swap(5,6);
+	new_state.swap(1, 2);
+	new_state.swap(5, 6);
 
-    new_state
+	new_state
 }
 
 #[inline]
@@ -96,23 +97,23 @@ fn get_substates_par(mut state: State) -> [[u8; STATE_SIZE]; NUM_PARALLEL_SUBSTA
 		)
 	};
 
-    for i in 0..8 {
-        if i % 8 < 4 {
-            let a = state[i];
-            let b = state[i + 4];
-            state[i] = unsafe { _mm256_permute2x128_si256(a, b, 0x20) };
-            state[i + 4] = unsafe { _mm256_permute2x128_si256(a, b, 0x31) };
-        }
-    }
+	for i in 0..8 {
+		if i % 8 < 4 {
+			let a = state[i];
+			let b = state[i + 4];
+			state[i] = unsafe { _mm256_permute2x128_si256(a, b, 0x20) };
+			state[i + 4] = unsafe { _mm256_permute2x128_si256(a, b, 0x31) };
+		}
+	}
 
-    for i in 0..8 {
-        if i % 4 < 2 {
-            let a = state[i];
-            let b = state[i + 2];
-            state[i] = unsafe { _mm256_unpacklo_epi64(a, b) };
-            state[i + 2] = unsafe { _mm256_unpackhi_epi64(a, b) };
-        }
-    }
+	for i in 0..8 {
+		if i % 4 < 2 {
+			let a = state[i];
+			let b = state[i + 2];
+			state[i] = unsafe { _mm256_unpacklo_epi64(a, b) };
+			state[i + 2] = unsafe { _mm256_unpackhi_epi64(a, b) };
+		}
+	}
 
 	for state_row in &mut state {
 		let permuted = unsafe { _mm256_shuffle_epi8(*state_row, unbyteslice_permutation_m256) };
@@ -155,257 +156,256 @@ fn get_substates_par(mut state: State) -> [[u8; STATE_SIZE]; NUM_PARALLEL_SUBSTA
 
 #[inline]
 fn add_round_constant_p(input: &mut State, round: i8) {
-    let broadcasted_first_row = unsafe { _mm256_set1_epi64x(0x7060504030201000) };
-    let round_dependent = unsafe { _mm256_set1_epi8(round) };
-    let whole_round_constant = unsafe { _mm256_xor_si256(round_dependent, broadcasted_first_row) };
-    input[0] = unsafe { _mm256_xor_si256(input[0], whole_round_constant) };
+	let broadcasted_first_row = unsafe { _mm256_set1_epi64x(0x7060504030201000) };
+	let round_dependent = unsafe { _mm256_set1_epi8(round) };
+	let whole_round_constant = unsafe { _mm256_xor_si256(round_dependent, broadcasted_first_row) };
+	input[0] = unsafe { _mm256_xor_si256(input[0], whole_round_constant) };
 }
 
 #[inline]
 fn add_round_constant_q(input: &mut State, round: i8) {
-    let broadcasted_last_row = unsafe { _mm256_set1_epi64x(0x8f9fafbfcfdfefffu64 as i64) };
-    let broadcasted_ff = unsafe { _mm256_set1_epi8(0xffu8 as i8) };
+	let broadcasted_last_row = unsafe { _mm256_set1_epi64x(0x8f9fafbfcfdfefffu64 as i64) };
+	let broadcasted_ff = unsafe { _mm256_set1_epi8(0xffu8 as i8) };
 
-    let round_dependent = unsafe { _mm256_set1_epi8(round) };
-    let whole_round_constant = unsafe { _mm256_xor_si256(round_dependent, broadcasted_last_row) };
-    input[7] = unsafe { _mm256_xor_si256(input[7], whole_round_constant) };
+	let round_dependent = unsafe { _mm256_set1_epi8(round) };
+	let whole_round_constant = unsafe { _mm256_xor_si256(round_dependent, broadcasted_last_row) };
+	input[7] = unsafe { _mm256_xor_si256(input[7], whole_round_constant) };
 
-    for i in 0..7 {
-        input[i] = unsafe { _mm256_xor_si256(input[i], broadcasted_ff) };
-    }
+	for i in 0..7 {
+		input[i] = unsafe { _mm256_xor_si256(input[i], broadcasted_ff) };
+	}
 }
 
 #[inline]
 fn sub_bytes(state: &mut State) {
-    const SBOX_AFFINE: i64 = 0xf1e3c78f1f3e7cf8u64 as i64;
+	const SBOX_AFFINE: i64 = 0xf1e3c78f1f3e7cf8u64 as i64;
 
-    let a = unsafe { _mm256_set1_epi64x(SBOX_AFFINE) };
+	let a = unsafe { _mm256_set1_epi64x(SBOX_AFFINE) };
 
-    for i in 0..8 {
-        state[i] = unsafe { _mm256_gf2p8affineinv_epi64_epi8(state[i], a, 0b01100011) };
-    }
+	for i in 0..8 {
+		state[i] = unsafe { _mm256_gf2p8affineinv_epi64_epi8(state[i], a, 0b01100011) };
+	}
 }
 
 #[inline]
 fn shift_bytes_p(state: &mut State) {
-    state[0] = unsafe { _mm256_ror_epi64(state[0], 8 * 0) };
-    state[1] = unsafe { _mm256_ror_epi64(state[1], 8 * 1) };
-    state[2] = unsafe { _mm256_ror_epi64(state[2], 8 * 2) };
-    state[3] = unsafe { _mm256_ror_epi64(state[3], 8 * 3) };
-    state[4] = unsafe { _mm256_ror_epi64(state[4], 8 * 4) };
-    state[5] = unsafe { _mm256_ror_epi64(state[5], 8 * 5) };
-    state[6] = unsafe { _mm256_ror_epi64(state[6], 8 * 6) };
-    state[7] = unsafe { _mm256_ror_epi64(state[7], 8 * 7) };
+	state[0] = unsafe { _mm256_ror_epi64(state[0], 8 * 0) };
+	state[1] = unsafe { _mm256_ror_epi64(state[1], 8 * 1) };
+	state[2] = unsafe { _mm256_ror_epi64(state[2], 8 * 2) };
+	state[3] = unsafe { _mm256_ror_epi64(state[3], 8 * 3) };
+	state[4] = unsafe { _mm256_ror_epi64(state[4], 8 * 4) };
+	state[5] = unsafe { _mm256_ror_epi64(state[5], 8 * 5) };
+	state[6] = unsafe { _mm256_ror_epi64(state[6], 8 * 6) };
+	state[7] = unsafe { _mm256_ror_epi64(state[7], 8 * 7) };
 }
 
 fn shift_bytes_q(state: &mut State) {
-    state[0] = unsafe { _mm256_ror_epi64(state[0], 8 * 1) };
-    state[1] = unsafe { _mm256_ror_epi64(state[1], 8 * 3) };
-    state[2] = unsafe { _mm256_ror_epi64(state[2], 8 * 5) };
-    state[3] = unsafe { _mm256_ror_epi64(state[3], 8 * 7) };
-    state[4] = unsafe { _mm256_ror_epi64(state[4], 8 * 0) };
-    state[5] = unsafe { _mm256_ror_epi64(state[5], 8 * 2) };
-    state[6] = unsafe { _mm256_ror_epi64(state[6], 8 * 4) };
-    state[7] = unsafe { _mm256_ror_epi64(state[7], 8 * 6) };
+	state[0] = unsafe { _mm256_ror_epi64(state[0], 8 * 1) };
+	state[1] = unsafe { _mm256_ror_epi64(state[1], 8 * 3) };
+	state[2] = unsafe { _mm256_ror_epi64(state[2], 8 * 5) };
+	state[3] = unsafe { _mm256_ror_epi64(state[3], 8 * 7) };
+	state[4] = unsafe { _mm256_ror_epi64(state[4], 8 * 0) };
+	state[5] = unsafe { _mm256_ror_epi64(state[5], 8 * 2) };
+	state[6] = unsafe { _mm256_ror_epi64(state[6], 8 * 4) };
+	state[7] = unsafe { _mm256_ror_epi64(state[7], 8 * 6) };
 }
 
 #[inline]
 fn mix_bytes(state: &mut State) {
-    let mut x = [unsafe { _mm256_set1_epi64x(0) }; 8];
-    let mut y = [unsafe { _mm256_set1_epi64x(0) }; 8];
-    let mut z = [unsafe { _mm256_set1_epi64x(0) }; 8];
+	let mut x = [unsafe { _mm256_set1_epi64x(0) }; 8];
+	let mut y = [unsafe { _mm256_set1_epi64x(0) }; 8];
+	let mut z = [unsafe { _mm256_set1_epi64x(0) }; 8];
 
-    let _2: __m256i = unsafe { _mm256_set1_epi8(2) };
+	let _2: __m256i = unsafe { _mm256_set1_epi8(2) };
 
-    for i in 0..8 {
-        x[i] = unsafe { _mm256_xor_si256(state[i], state[(i + 1) % 8]) };
-    }
+	for i in 0..8 {
+		x[i] = unsafe { _mm256_xor_si256(state[i], state[(i + 1) % 8]) };
+	}
 
-    for i in 0..8 {
-        y[i] = unsafe { _mm256_xor_si256(x[i], x[(i + 3) % 8]) };
-    }
+	for i in 0..8 {
+		y[i] = unsafe { _mm256_xor_si256(x[i], x[(i + 3) % 8]) };
+	}
 
-    for i in 0..8 {
-        z[i] =
-            unsafe { _mm256_xor_si256(_mm256_xor_si256(x[i], x[(i + 2) % 8]), state[(i + 6) % 8]) };
-    }
+	for i in 0..8 {
+		z[i] =
+			unsafe { _mm256_xor_si256(_mm256_xor_si256(x[i], x[(i + 2) % 8]), state[(i + 6) % 8]) };
+	}
 
-    for i in 0..8 {
-        state[i] = unsafe {
-            _mm256_xor_si256(
-                _mm256_gf2p8mul_epi8(
-                    _2,
-                    _mm256_xor_si256(_mm256_gf2p8mul_epi8(_2, y[(i + 3) % 8]), z[(i + 7) % 8]),
-                ),
-                z[(i + 4) % 8],
-            )
-        };
-    }
+	for i in 0..8 {
+		state[i] = unsafe {
+			_mm256_xor_si256(
+				_mm256_gf2p8mul_epi8(
+					_2,
+					_mm256_xor_si256(_mm256_gf2p8mul_epi8(_2, y[(i + 3) % 8]), z[(i + 7) % 8]),
+				),
+				z[(i + 4) % 8],
+			)
+		};
+	}
 }
 
 fn permutation_p(state: &mut State) {
-    for r in 0..ROUNDS_PER_PERMUTATION {
-        add_round_constant_p(state, r as i8);
-        sub_bytes(state);
-        shift_bytes_p(state);
-        mix_bytes(state);
-    }
+	for r in 0..ROUNDS_PER_PERMUTATION {
+		add_round_constant_p(state, r as i8);
+		sub_bytes(state);
+		shift_bytes_p(state);
+		mix_bytes(state);
+	}
 }
 
 fn permutation_q(state: &mut State) {
-    for r in 0..ROUNDS_PER_PERMUTATION {
-        add_round_constant_q(state, r as i8);
-        sub_bytes(state);
-        shift_bytes_q(state);
-        mix_bytes(state);
-    }
+	for r in 0..ROUNDS_PER_PERMUTATION {
+		add_round_constant_q(state, r as i8);
+		sub_bytes(state);
+		shift_bytes_q(state);
+		mix_bytes(state);
+	}
 }
 
 #[derive(Clone)]
 pub struct Groestl256Multi {
-    state: State,
+	state: State,
 }
 
 impl Groestl256Multi {
-    fn consume_single_block_parallel(&mut self, data: [&[u8]; 4]) {
+	fn consume_single_block_parallel(&mut self, data: [&[u8]; 4]) {
+		let mut q_data = set_substates_par(data);
 
-        let mut q_data = set_substates_par(data);
+		let mut p_data = [unsafe { _mm256_set1_epi64x(0) }; 8];
 
-        let mut p_data = [unsafe { _mm256_set1_epi64x(0) }; 8];
+		for i in 0..8 {
+			p_data[i] = unsafe { _mm256_xor_si256(self.state[i], q_data[i]) };
+		}
 
-        for i in 0..8 {
-            p_data[i] = unsafe { _mm256_xor_si256(self.state[i], q_data[i]) };
-        }
+		permutation_p(&mut p_data);
+		permutation_q(&mut q_data);
 
-        permutation_p(&mut p_data);
-        permutation_q(&mut q_data);
+		for i in 0..8 {
+			self.state[i] =
+				unsafe { _mm256_xor_si256(_mm256_xor_si256(self.state[i], q_data[i]), p_data[i]) };
+		}
+	}
 
-        for i in 0..8 {
-            self.state[i] =
-                unsafe { _mm256_xor_si256(_mm256_xor_si256(self.state[i], q_data[i]), p_data[i]) };
-        }
-    }
+	fn finalize(&mut self, out: &mut [MaybeUninit<digest::Output<Groestl256>>; 4]) {
+		let state_copy = self.state.clone();
+		permutation_p(&mut self.state);
+		for i in 0..8 {
+			self.state[i] = unsafe { _mm256_xor_si256(self.state[i], state_copy[i]) };
+		}
 
-    fn finalize(&mut self, out: &mut [MaybeUninit<digest::Output<Groestl256>>; 4]) {
-        let state_copy = self.state.clone();
-        permutation_p(&mut self.state);
-        for i in 0..8 {
-            self.state[i] = unsafe { _mm256_xor_si256(self.state[i], state_copy[i]) };
-        }
+		let slices = get_substates_par_better(self.state);
 
-        let slices = get_substates_par_better(self.state);
-
-        for parallel_idx in 0..NUM_PARALLEL_SUBSTATES {
-            let slice = slices[parallel_idx];
-            unsafe {
-                out[parallel_idx]
-                    .as_mut_ptr()
-                    .write(*digest::Output::<Groestl256>::from_slice(&slice[32..]));
-            }
-        }
-    }
+		for parallel_idx in 0..NUM_PARALLEL_SUBSTATES {
+			let slice = slices[parallel_idx];
+			unsafe {
+				out[parallel_idx]
+					.as_mut_ptr()
+					.write(*digest::Output::<Groestl256>::from_slice(&slice[32..]));
+			}
+		}
+	}
 }
 
 impl Default for Groestl256Multi {
-    fn default() -> Self {
-        // seeding initial states with the 512b representation of 256
-        Self {
-            state: [
-                unsafe { _mm256_set1_epi64x(0) },
-                unsafe { _mm256_set1_epi64x(0) },
-                unsafe { _mm256_set1_epi64x(0) },
-                unsafe { _mm256_set1_epi64x(0) },
-                unsafe { _mm256_set1_epi64x(0) },
-                unsafe { _mm256_set1_epi64x(0) },
-                unsafe { _mm256_set1_epi64x(0x100000000000000u64 as i64) },
-                unsafe { _mm256_set1_epi64x(0) },
-            ],
-        }
-    }
+	fn default() -> Self {
+		// seeding initial states with the 512b representation of 256
+		Self {
+			state: [
+				unsafe { _mm256_set1_epi64x(0) },
+				unsafe { _mm256_set1_epi64x(0) },
+				unsafe { _mm256_set1_epi64x(0) },
+				unsafe { _mm256_set1_epi64x(0) },
+				unsafe { _mm256_set1_epi64x(0) },
+				unsafe { _mm256_set1_epi64x(0) },
+				unsafe { _mm256_set1_epi64x(0x100000000000000u64 as i64) },
+				unsafe { _mm256_set1_epi64x(0) },
+			],
+		}
+	}
 }
 
 impl MultiDigest<4> for Groestl256Multi {
-    type Digest = Groestl256;
+	type Digest = Groestl256;
 
-    fn new() -> Self {
-        Self::default()
-    }
+	fn new() -> Self {
+		Self::default()
+	}
 
-    fn update(&mut self, data: [&[u8]; 4]) {
-        for parallel_idx in 1..NUM_PARALLEL_SUBSTATES {
-            assert_eq!(data[parallel_idx].len(), data[0].len());
-        }
+	fn update(&mut self, data: [&[u8]; 4]) {
+		for parallel_idx in 1..NUM_PARALLEL_SUBSTATES {
+			assert_eq!(data[parallel_idx].len(), data[0].len());
+		}
 
-        let mut i = 0;
+		let mut i = 0;
 
-        while i + 64 <= data[0].len() {
-            self.consume_single_block_parallel([
-                &data[0][i..i + 64],
-                &data[1][i..i + 64],
-                &data[2][i..i + 64],
-                &data[3][i..i + 64],
-            ]);
+		while i + 64 <= data[0].len() {
+			self.consume_single_block_parallel([
+				&data[0][i..i + 64],
+				&data[1][i..i + 64],
+				&data[2][i..i + 64],
+				&data[3][i..i + 64],
+			]);
 
-            i += 64;
-        }
+			i += 64;
+		}
 
-        // now we're at the first non-completely-full block
-        let mut this_data: [[u8; 64]; 4] = [[0u8; 64]; 4];
-        let mut next_data: [[u8; 64]; 4] = [[0u8; 64]; 4];
+		// now we're at the first non-completely-full block
+		let mut this_data: [[u8; 64]; 4] = [[0u8; 64]; 4];
+		let mut next_data: [[u8; 64]; 4] = [[0u8; 64]; 4];
 
-        let no_additional_block = data[0].len() % 64 < 56;
+		let no_additional_block = data[0].len() % 64 < 56;
 
-        for parallel_idx in 0..NUM_PARALLEL_SUBSTATES {
-            let this_instance_data = data[parallel_idx];
-            let mut this_block: [u8; 64] = [0; 64];
-            let mut next_block: [u8; 64] = [0; 64];
+		for parallel_idx in 0..NUM_PARALLEL_SUBSTATES {
+			let this_instance_data = data[parallel_idx];
+			let mut this_block: [u8; 64] = [0; 64];
+			let mut next_block: [u8; 64] = [0; 64];
 
-            this_block[0..this_instance_data.len() - i]
-                .copy_from_slice(&this_instance_data[i..this_instance_data.len()]);
-            
-            this_block[this_instance_data.len() - i] = 0b10000000;
+			this_block[0..this_instance_data.len() - i]
+				.copy_from_slice(&this_instance_data[i..this_instance_data.len()]);
 
-            if no_additional_block {
-                this_block[56..64].copy_from_slice(&((i / 64 + 1) as u64).to_be_bytes());
-            } else {
-                next_block[56..64].copy_from_slice(&((i / 64 + 2) as u64).to_be_bytes());
-                next_data[parallel_idx] = next_block;
-            }
-            this_data[parallel_idx] = this_block;
-        }
+			this_block[this_instance_data.len() - i] = 0b10000000;
 
-        self.consume_single_block_parallel([
-            &this_data[0],
-            &this_data[1],
-            &this_data[2],
-            &this_data[3],
-        ]);
-        if !no_additional_block {
-            self.consume_single_block_parallel([
-                &next_data[0],
-                &next_data[1],
-                &next_data[2],
-                &next_data[3],
-            ]);
-        }
-    }
+			if no_additional_block {
+				this_block[56..64].copy_from_slice(&((i / 64 + 1) as u64).to_be_bytes());
+			} else {
+				next_block[56..64].copy_from_slice(&((i / 64 + 2) as u64).to_be_bytes());
+				next_data[parallel_idx] = next_block;
+			}
+			this_data[parallel_idx] = this_block;
+		}
 
-    fn finalize_into(mut self, out: &mut [MaybeUninit<digest::Output<Self::Digest>>; 4]) {
-        self.finalize(out)
-    }
+		self.consume_single_block_parallel([
+			&this_data[0],
+			&this_data[1],
+			&this_data[2],
+			&this_data[3],
+		]);
+		if !no_additional_block {
+			self.consume_single_block_parallel([
+				&next_data[0],
+				&next_data[1],
+				&next_data[2],
+				&next_data[3],
+			]);
+		}
+	}
 
-    fn finalize_into_reset(&mut self, out: &mut [MaybeUninit<digest::Output<Self::Digest>>; 4]) {
-        self.finalize(out);
-        self.reset();
-    }
+	fn finalize_into(mut self, out: &mut [MaybeUninit<digest::Output<Self::Digest>>; 4]) {
+		self.finalize(out)
+	}
 
-    fn reset(&mut self) {
-        self.state = Self::default().state;
-    }
+	fn finalize_into_reset(&mut self, out: &mut [MaybeUninit<digest::Output<Self::Digest>>; 4]) {
+		self.finalize(out);
+		self.reset();
+	}
 
-    fn digest(data: [&[u8]; 4], out: &mut [MaybeUninit<digest::Output<Self::Digest>>; 4]) {
-        let mut digest = Self::default();
-        digest.update(data);
-        digest.finalize_into(out);
-    }
+	fn reset(&mut self) {
+		self.state = Self::default().state;
+	}
+
+	fn digest(data: [&[u8]; 4], out: &mut [MaybeUninit<digest::Output<Self::Digest>>; 4]) {
+		let mut digest = Self::default();
+		digest.update(data);
+		digest.finalize_into(out);
+	}
 }
