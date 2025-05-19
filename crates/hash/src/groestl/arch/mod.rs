b792fb6f600489c2230b0151cdb57dc7bbe71ea5
jsk11235 <kirmayerjacob@gmail.com>
2025-05-19 11:08:11 +0200

    clean

--- crates/hash/src/groestl/arch/mod.rs
+++ crates/hash/src/groestl/arch/mod.rs
@@ -6,7 +6,6 @@ use cfg_if::cfg_if;
 // extensions, otherwise defaults to the portable implementation which was found to be fast in most
 // machines
 
-
 cfg_if! {
 	if #[cfg(all(feature = "nightly_features", target_arch = "x86_64",target_feature = "avx512bw",target_feature = "avx512vbmi",target_feature = "avx512f",target_feature = "gfni",))] {
 		mod groestl_avx512;
