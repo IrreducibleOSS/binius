bbd1328aeaa99ba1f8d17ed80a3b117535eda92b
pepyakin <sshulepov@irreducible.com>
2025-06-06 11:53:37 +0000

    [circuits] Kill it. (#613)
    Good night sweet prince, you served well.

--- crates/circuits/src/builder/types.rs
+++
@@ -1,4 +0,0 @@
-// Copyright 2025 Irreducible Inc.
-
-pub type F = binius_field::BinaryField128b;
-pub type U = binius_field::arch::OptimalUnderlier;
--- crates/circuits/src/lasso/lookups/mod.rs
+++
@@ -1,3 +0,0 @@
-// Copyright 2024-2025 Irreducible Inc.
-
-pub mod u8_arithmetic;
