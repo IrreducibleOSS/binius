dc9d0c1140ffaf223f885df530d8dc303aa632f5
jsk11235 <kirmayerjacob@gmail.com>
2025-06-09 12:34:48 +0200

    compute layer fri folder

--- crates/core/src/protocols/fri/prove.rs
+++ crates/core/src/protocols/fri/prove.rs
@@ -1,11 +1,18 @@
 // Copyright 2024-2025 Irreducible Inc.
-
+use binius_compute::{
+	ComputeLayerExecutor,
+	alloc::ComputeAllocator,
+	layer::ComputeLayer,
+	memory::{ComputeMemory, SizedSlice},
+};
 use binius_field::{
 	BinaryField, ExtensionField, PackedExtension, PackedField, TowerField,
 	packed::{iter_packed_slice_with_offset, len_packed_slice},
+	unpack_if_possible,
 };
+use binius_math::MultilinearQuery;
 use binius_maybe_rayon::prelude::*;
-use binius_ntt::{AdditiveNTT, fri::fold_interleaved};
+use binius_ntt::{AdditiveNTT, fri::fold_interleaved_chunk};
 use binius_utils::{SerializeBytes, bail, checked_arithmetics::log2_strict_usize};
 use bytemuck::zeroed_vec;
 use bytes::BufMut;
@@ -25,6 +32,63 @@ use crate::{
 	reed_solomon::reed_solomon::ReedSolomonCode,
 	transcript::{ProverTranscript, TranscriptWriter},
 };
+
+/// FRI-fold the interleaved codeword using the given challenges.
+///
+/// ## Arguments
+///
+/// * `ntt` - the NTT instance, used to look up the twiddle values.
+/// * `codeword` - an interleaved codeword.
+/// * `challenges` - the folding challenges. The length must be at least `log_batch_size`.
+/// * `log_len` - the binary logarithm of the code length.
+/// * `log_batch_size` - the binary logarithm of the interleaved code batch size.
+///
+/// See [DP24], Def. 3.6 and Lemma 3.9 for more details.
+///
+/// [DP24]: <https://eprint.iacr.org/2024/504>
+#[instrument(skip_all, level = "debug")]
+pub fn fold_interleaved<F, FS, NTT, P>(
+	ntt: &NTT,
+	codeword: &[P],
+	challenges: &[F],
+	log_len: usize,
+	log_batch_size: usize,
+) -> Vec<F>
+where
+	F: BinaryField + ExtensionField<FS>,
+	FS: BinaryField,
+	NTT: AdditiveNTT<FS> + Sync,
+	P: PackedField<Scalar = F>,
+{
+	assert_eq!(codeword.len(), 1 << (log_len + log_batch_size).saturating_sub(P::LOG_WIDTH));
+	assert!(challenges.len() >= log_batch_size);
+
+	let (interleave_challenges, fold_challenges) = challenges.split_at(log_batch_size);
+	let tensor = MultilinearQuery::expand(interleave_challenges);
+
+	// For each chunk of size `2^chunk_size` in the codeword, fold it with the folding challenges
+	let fold_chunk_size = 1 << fold_challenges.len();
+	let chunk_size = 1 << challenges.len().saturating_sub(P::LOG_WIDTH);
+	codeword
+		.par_chunks(chunk_size)
+		.enumerate()
+		.map_init(
+			|| vec![F::default(); fold_chunk_size],
+			|scratch_buffer, (i, chunk)| {
+				fold_interleaved_chunk(
+					ntt,
+					log_len,
+					log_batch_size,
+					i,
+					chunk,
+					tensor.expansion(),
+					fold_challenges,
+					scratch_buffer,
+				)
+			},
+		)
+		.collect()
+}
 
 #[derive(Debug)]
 pub struct CommitOutput<P, VCSCommitment, VCSCommitted> {
@@ -403,6 +467,294 @@ where
 			merkle_prover,
 			..
 		} = self;
+
+		let query_prover = FRIQueryProver {
+			params,
+			codeword,
+			codeword_committed,
+			round_committed,
+			merkle_prover,
+		};
+		Ok((terminate_codeword, query_prover))
+	}
+
+	pub fn finish_proof<Challenger_>(
+		self,
+		transcript: &mut ProverTranscript<Challenger_>,
+	) -> Result<(), Error>
+	where
+		Challenger_: Challenger,
+	{
+		let (terminate_codeword, query_prover) = self.finalize()?;
+		let mut advice = transcript.decommitment();
+		advice.write_scalar_slice(&terminate_codeword);
+
+		let layers = query_prover.vcs_optimal_layers()?;
+		for layer in layers {
+			advice.write_slice(&layer);
+		}
+
+		let params = query_prover.params;
+
+		for _ in 0..params.n_test_queries() {
+			let index = transcript.sample_bits(params.index_bits()) as usize;
+			query_prover.prove_query(index, transcript.decommitment())?;
+		}
+
+		Ok(())
+	}
+}
+
+pub struct FRIFolderCL<'a, 'b, F, FA, P, NTT, MerkleProver, VCS, CL>
+where
+	FA: BinaryField,
+	F: BinaryField,
+	P: PackedField<Scalar = F>,
+	MerkleProver: MerkleTreeProver<F, Scheme = VCS>,
+	VCS: MerkleTreeScheme<F>,
+	CL: ComputeLayer<F>,
+{
+	params: &'a FRIParams<F, FA>,
+	ntt: &'a NTT,
+	merkle_prover: &'a MerkleProver,
+	codeword: &'a [P],
+	codeword_committed: &'a MerkleProver::Committed,
+	round_committed:
+		Vec<(Vec<F>, <CL::DevMem as ComputeMemory<F>>::FSliceMut<'b>, MerkleProver::Committed)>,
+	curr_round: usize,
+	next_commit_round: Option<usize>,
+	unprocessed_challenges: Vec<F>,
+	cl: &'b CL,
+}
+
+impl<'a, 'b, F, FA, P, NTT, MerkleProver, VCS, CL>
+	FRIFolderCL<'a, 'b, F, FA, P, NTT, MerkleProver, VCS, CL>
+where
+	F: TowerField + ExtensionField<FA>,
+	FA: BinaryField,
+	P: PackedField<Scalar = F>,
+	NTT: AdditiveNTT<FA> + Sync,
+	MerkleProver: MerkleTreeProver<F, Scheme = VCS>,
+	VCS: MerkleTreeScheme<F, Digest: SerializeBytes>,
+	CL: ComputeLayer<F>,
+{
+	/// Constructs a new folder.
+	pub fn new(
+		params: &'a FRIParams<F, FA>,
+		ntt: &'a NTT,
+		merkle_prover: &'a MerkleProver,
+		committed_codeword: &'a [P],
+		committed: &'a MerkleProver::Committed,
+		cl: &'b CL,
+	) -> Result<Self, Error> {
+		if len_packed_slice(committed_codeword) < 1 << params.log_len() {
+			bail!(Error::InvalidArgs(
+				"Reed–Solomon code length must match interleaved codeword length".to_string(),
+			));
+		}
+
+		let next_commit_round = params.fold_arities().first().copied();
+		Ok(Self {
+			params,
+			ntt,
+			merkle_prover,
+			codeword: committed_codeword,
+			codeword_committed: committed,
+			round_committed: Vec::with_capacity(params.n_oracles()),
+			curr_round: 0,
+			next_commit_round,
+			unprocessed_challenges: Vec::with_capacity(params.rs_code().log_dim()),
+			cl,
+		})
+	}
+
+	/// Number of fold rounds, including the final fold.
+	pub const fn n_rounds(&self) -> usize {
+		self.params.n_fold_rounds()
+	}
+
+	/// Number of times `execute_fold_round` has been called.
+	pub const fn curr_round(&self) -> usize {
+		self.curr_round
+	}
+
+	/// The length of the current codeword.
+	pub fn current_codeword_len(&self) -> usize {
+		match self.round_committed.last() {
+			Some((codeword, _, _)) => codeword.len(),
+			None => len_packed_slice(self.codeword),
+		}
+	}
+
+	fn is_commitment_round(&self) -> bool {
+		self.next_commit_round
+			.is_some_and(|round| round == self.curr_round)
+	}
+
+	/// Executes the next fold round and returns the folded codeword commitment.
+	///
+	/// As a memory efficient optimization, this method may not actually do the folding, but instead
+	/// accumulate the folding challenge for processing at a later time. This saves us from storing
+	/// intermediate folded codewords.
+	pub fn execute_fold_round(
+		&mut self,
+		allocator: &'b impl ComputeAllocator<F, CL::DevMem>,
+		challenge: F,
+	) -> Result<FoldRoundOutput<VCS::Digest>, Error> {
+		self.unprocessed_challenges.push(challenge);
+		self.curr_round += 1;
+
+		if !self.is_commitment_round() {
+			return Ok(FoldRoundOutput::NoCommitment);
+		}
+
+		let dimensions_data = match self.round_committed.last() {
+			Some((codeword, _, _)) => FRIFoldData::new::<F, FA>(
+				log2_strict_usize(codeword.len()),
+				0,
+				self.unprocessed_challenges.len(),
+			),
+			None => FRIFoldData::new::<F, FA>(
+				self.params.rs_code().log_len(),
+				self.params.log_batch_size(),
+				self.unprocessed_challenges.len(),
+			),
+		};
+
+		let fri_fold_span = tracing::debug_span!(
+			"[task] FRI Fold",
+			phase = "piop_compiler",
+			perfetto_category = "task.main",
+			dimensions_data = ?dimensions_data
+		)
+		.entered();
+		// Fold the last codeword with the accumulated folding challenges.
+		let folded_codeword = match self.round_committed.last() {
+			Some((_, prev_codeword, _)) => {
+				// Fold a full codeword committed in the previous FRI round into a codeword with
+				// reduced dimension and rate.
+
+				let mut folded_codeword =
+					allocator.alloc(prev_codeword.len() - self.unprocessed_challenges.len())?;
+				self.cl.execute(|exec| {
+					exec.fri_fold(
+						self.ntt,
+						log2_strict_usize(prev_codeword.len()),
+						0,
+						&self.unprocessed_challenges,
+						CL::DevMem::as_const(prev_codeword),
+						&mut folded_codeword,
+					)?;
+
+					Ok(vec![])
+				})?;
+
+				folded_codeword
+			}
+			None => {
+				let codeword_len = len_packed_slice(self.codeword);
+				let mut original_codeword = allocator.alloc(codeword_len)?;
+				unpack_if_possible(
+					self.codeword,
+					|scalars| self.cl.copy_h2d(scalars, &mut original_codeword),
+					|_packed| unimplemented!("non-dense packed fields not supported"),
+				)?;
+				let mut folded_codeword = allocator.alloc(
+					1 << (self.params.rs_code().log_len()
+						- (self.unprocessed_challenges.len() - self.params.log_batch_size())),
+				)?;
+				self.cl.execute(|exec| {
+					exec.fri_fold(
+						self.ntt,
+						self.params.rs_code().log_len(),
+						self.params.log_batch_size(),
+						&self.unprocessed_challenges,
+						CL::DevMem::as_const(&original_codeword),
+						&mut folded_codeword,
+					)?;
+
+					Ok(vec![])
+				})?;
+
+				folded_codeword
+			}
+		};
+		drop(fri_fold_span);
+		self.unprocessed_challenges.clear();
+
+		let mut folded_codeword_host = zeroed_vec(folded_codeword.len());
+		self.cl
+			.copy_d2h(CL::DevMem::as_const(&folded_codeword), &mut folded_codeword_host)?;
+
+		// take the first arity as coset_log_len, or use inv_rate if arities are empty
+		let coset_size = self
+			.params
+			.fold_arities()
+			.get(self.round_committed.len() + 1)
+			.map(|log| 1 << log)
+			.unwrap_or_else(|| 1 << self.params.n_final_challenges());
+		let dimension_data =
+			MerkleTreeDimensionData::new::<F>(dimensions_data.log_len(), coset_size);
+		let merkle_tree_span = tracing::debug_span!(
+			"[task] Merkle Tree",
+			phase = "piop_compiler",
+			perfetto_category = "task.main",
+			dimensions_data = ?dimension_data
+		)
+		.entered();
+		let (commitment, committed) = self
+			.merkle_prover
+			.commit(&folded_codeword_host, coset_size)
+			.map_err(|err| Error::VectorCommit(Box::new(err)))?;
+		drop(merkle_tree_span);
+
+		self.round_committed
+			.push((folded_codeword_host, folded_codeword, committed));
+
+		self.next_commit_round = self.next_commit_round.take().and_then(|next_commit_round| {
+			let arity = self.params.fold_arities().get(self.round_committed.len())?;
+			Some(next_commit_round + arity)
+		});
+		Ok(FoldRoundOutput::Commitment(commitment.root))
+	}
+
+	/// Finalizes the FRI folding process.
+	///
+	/// This step will process any unprocessed folding challenges to produce the
+	/// final folded codeword. Then it will decode this final folded codeword
+	/// to get the final message. The result is the final message and a query prover instance.
+	///
+	/// This returns the final message and a query prover instance.
+	#[instrument(skip_all, name = "fri::FRIFolder::finalize", level = "debug")]
+	#[allow(clippy::type_complexity)]
+	pub fn finalize(
+		mut self,
+	) -> Result<(TerminateCodeword<F>, FRIQueryProver<'a, F, FA, P, MerkleProver, VCS>), Error> {
+		if self.curr_round != self.n_rounds() {
+			bail!(Error::EarlyProverFinish);
+		}
+
+		let terminate_codeword = self
+			.round_committed
+			.last()
+			.map(|(codeword, _, _)| codeword.clone())
+			.unwrap_or_else(|| PackedField::iter_slice(self.codeword).collect());
+
+		self.unprocessed_challenges.clear();
+
+		let Self {
+			params,
+			codeword,
+			codeword_committed,
+			round_committed,
+			merkle_prover,
+			..
+		} = self;
+
+		let round_committed = round_committed
+			.into_iter()
+			.map(|(codeword, _, committed)| (codeword, committed))
+			.collect();
 
 		let query_prover = FRIQueryProver {
 			params,
